d77c1bda400ec325fa171d84e365137ee51747d3
James Cotton <peabody124@gmail.com>
2011-04-16 08:14:14 -0500

    Add the ADC channels to the config file, also removed some unused things and added the delay channel

--- flight/PiOS/Boards/STM32F103CB_ESC_Rev1.h
+++ flight/PiOS/Boards/STM32F103CB_ESC_Rev1.h
@@ -33,10 +33,11 @@
 /*
 Timer | Channel 1 | Channel 2 | Channel 3 | Channel 4
 ------+-----------+-----------+-----------+----------
-TIM1  |  Servo 4  |           |           |
-TIM2  |  RC In 5  |  RC In 6  |  Servo 6  |  
-TIM3  |  Servo 5  |  RC In 2  |  RC In 3  |  RC In 4
-TIM4  |  RC In 1  |  Servo 3  |  Servo 2  |  Servo 1
+TIM1  |           |           |           |
+TIM2  |    B-     |     B+    |     A-    |     A+    
+TIM3  |    C-     |     C+    |  RC In 3  |  RC In 4
+TIM4  |                     DELAY 
+TIM16 |  PWM in   |           |           |  
 ------+-----------+-----------+-----------+----------
 */
 
@@ -93,11 +94,6 @@ TIM4  |  RC In 1  |  Servo 3  |  Servo 2  |  Servo 1
 #define PIOS_WDG_MANUAL          0x0008
 
 //------------------------
-// TELEMETRY 
-//------------------------
-#define TELEM_QUEUE_SIZE         20
-
-//------------------------
 // PIOS_LED
 //------------------------
 #define PIOS_LED_LED1_GPIO_PORT			GPIOB
@@ -115,10 +111,72 @@ TIM4  |  RC In 1  |  Servo 3  |  Servo 2  |  Servo 1
 #define PIOS_LED_CLKS				{ PIOS_LED_LED1_GPIO_CLK, PIOS_LED_LED2_GPIO_CLK, PIOS_LED_LED3_GPIO_CLK }
 
 //-------------------------
+// ADC
+// PIOS_ADC_PinGet(0) = Gyro Z
+// PIOS_ADC_PinGet(1) = Gyro Y
+// PIOS_ADC_PinGet(2) = Gyro X
+//-------------------------
+//#define PIOS_ADC_OVERSAMPLING_RATE		1
+#define PIOS_ADC_USE_TEMP_SENSOR		1
+#define PIOS_ADC_TEMP_SENSOR_ADC		ADC1
+#define PIOS_ADC_TEMP_SENSOR_ADC_CHANNEL	1
+
+// Phase A (PA6)
+#define PIOS_ADC_PIN1_GPIO_PORT			GPIOA			
+#define PIOS_ADC_PIN1_GPIO_PIN			GPIO_Pin_6		
+#define PIOS_ADC_PIN1_GPIO_CHANNEL		ADC_Channel_6
+#define PIOS_ADC_PIN1_ADC			ADC2
+#define PIOS_ADC_PIN1_ADC_NUMBER		1
+
+// Phase B (PA5)
+#define PIOS_ADC_PIN2_GPIO_PORT			GPIOA	
+#define PIOS_ADC_PIN2_GPIO_PIN			GPIO_Pin_5
+#define PIOS_ADC_PIN2_GPIO_CHANNEL		ADC_Channel_5
+#define PIOS_ADC_PIN2_ADC			ADC1
+#define PIOS_ADC_PIN2_ADC_NUMBER		2
+
+// Phase C (PA4)
+#define PIOS_ADC_PIN3_GPIO_PORT			GPIOA
+#define PIOS_ADC_PIN3_GPIO_PIN			GPIO_Pin_4
+#define PIOS_ADC_PIN3_GPIO_CHANNEL		ADC_Channel_4
+#define PIOS_ADC_PIN3_ADC			ADC2
+#define PIOS_ADC_PIN3_ADC_NUMBER		2
+
+#define PIOS_ADC_NUM_PINS			3
+
+#define PIOS_ADC_PORTS				{ PIOS_ADC_PIN1_GPIO_PORT, PIOS_ADC_PIN2_GPIO_PORT, PIOS_ADC_PIN3_GPIO_PORT }
+#define PIOS_ADC_PINS				{ PIOS_ADC_PIN1_GPIO_PIN, PIOS_ADC_PIN2_GPIO_PIN, PIOS_ADC_PIN3_GPIO_PIN }
+#define PIOS_ADC_CHANNELS			{ PIOS_ADC_PIN1_GPIO_CHANNEL, PIOS_ADC_PIN2_GPIO_CHANNEL, PIOS_ADC_PIN3_GPIO_CHANNEL }
+#define PIOS_ADC_MAPPING			{ PIOS_ADC_PIN1_ADC, PIOS_ADC_PIN2_ADC, PIOS_ADC_PIN3_ADC }
+#define PIOS_ADC_CHANNEL_MAPPING		{ PIOS_ADC_PIN1_ADC_NUMBER, PIOS_ADC_PIN2_ADC_NUMBER, PIOS_ADC_PIN3_ADC_NUMBER }
+#define PIOS_ADC_NUM_CHANNELS			(PIOS_ADC_NUM_PINS + PIOS_ADC_USE_TEMP_SENSOR) 
+#define PIOS_ADC_NUM_ADC_CHANNELS		2
+#define PIOS_ADC_USE_ADC2			1
+#define PIOS_ADC_CLOCK_FUNCTION			RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_ADC2, ENABLE)
+#define PIOS_ADC_ADCCLK				RCC_PCLK2_Div8
+/* RCC_PCLK2_Div2: ADC clock = PCLK2/2 */
+/* RCC_PCLK2_Div4: ADC clock = PCLK2/4 */
+/* RCC_PCLK2_Div6: ADC clock = PCLK2/6 */
+/* RCC_PCLK2_Div8: ADC clock = PCLK2/8 */
+#define PIOS_ADC_SAMPLE_TIME			ADC_SampleTime_71Cycles5
+/* Sample time: */
+/* With an ADCCLK = 14 MHz and a sampling time of 239.5 cycles: */
+/* Tconv = 71.5 + 12.5 = 84 cycles = 18�s */
+/* (1 / (ADCCLK / CYCLES)) = Sample Time (�S) */
+#define PIOS_ADC_IRQ_PRIO			PIOS_IRQ_PRIO_LOW
+
+// Currently analog acquistion hard coded at 480 Hz
+// PCKL2 = HCLK / 16
+// ADCCLK = PCLK2 / 2
+#define PIOS_ADC_RATE		(72.0e6 / 1.0 / 8.0 / 84.0 / (PIOS_ADC_NUM_CHANNELS >> PIOS_ADC_USE_ADC2))
+#define PIOS_ADC_MAX_OVERSAMPLING               36
+
+
+//-------------------------
 // Delay Timer
 //-------------------------
 #define PIOS_DELAY_TIMER			TIM4
-#define PIOS_DELAY_TIMER_RCC_FUNC		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE)
+#define PIOS_DELAY_TIMER_RCC_FUNC		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE)
 
 //-------------------------
 // System Settings
@@ -167,71 +225,11 @@ extern uint32_t pios_i2c_main_adapter_id;
 //
 // See also pios_board.c
 //-------------------------
-#define PIOS_COM_MAX_DEVS			4
+#define PIOS_COM_MAX_DEVS			1
 
-#define PIOS_COM_TELEM_BAUDRATE         57600
 extern uint32_t pios_com_debug_id;
 #define PIOS_COM_TELEM_RF               (pios_com_debug_id)
 #define PIOS_COM_DEBUG                  PIOS_COM_TELEM_RF
-
-
-//-------------------------
-// ADC
-// PIOS_ADC_PinGet(0) = Gyro Z
-// PIOS_ADC_PinGet(1) = Gyro Y
-// PIOS_ADC_PinGet(2) = Gyro X
-//-------------------------
-//#define PIOS_ADC_OVERSAMPLING_RATE		1
-#define PIOS_ADC_USE_TEMP_SENSOR		1
-#define PIOS_ADC_TEMP_SENSOR_ADC		ADC1
-#define PIOS_ADC_TEMP_SENSOR_ADC_CHANNEL	1
-
-#define PIOS_ADC_PIN1_GPIO_PORT			GPIOA			// PA4 (Gyro X)
-#define PIOS_ADC_PIN1_GPIO_PIN			GPIO_Pin_4		// ADC12_IN4
-#define PIOS_ADC_PIN1_GPIO_CHANNEL		ADC_Channel_4
-#define PIOS_ADC_PIN1_ADC			ADC2
-#define PIOS_ADC_PIN1_ADC_NUMBER		1
-
-#define PIOS_ADC_PIN2_GPIO_PORT			GPIOA			// PA5 (Gyro Y)
-#define PIOS_ADC_PIN2_GPIO_PIN			GPIO_Pin_5		// ADC123_IN5
-#define PIOS_ADC_PIN2_GPIO_CHANNEL		ADC_Channel_5
-#define PIOS_ADC_PIN2_ADC			ADC1
-#define PIOS_ADC_PIN2_ADC_NUMBER		2
-
-#define PIOS_ADC_PIN3_GPIO_PORT			GPIOA			// PA3 (Gyro Z)
-#define PIOS_ADC_PIN3_GPIO_PIN			GPIO_Pin_3		// ADC12_IN3
-#define PIOS_ADC_PIN3_GPIO_CHANNEL		ADC_Channel_3
-#define PIOS_ADC_PIN3_ADC			ADC2
-#define PIOS_ADC_PIN3_ADC_NUMBER		2
-
-#define PIOS_ADC_NUM_PINS			3
-
-#define PIOS_ADC_PORTS				{ PIOS_ADC_PIN1_GPIO_PORT, PIOS_ADC_PIN2_GPIO_PORT, PIOS_ADC_PIN3_GPIO_PORT }
-#define PIOS_ADC_PINS				{ PIOS_ADC_PIN1_GPIO_PIN, PIOS_ADC_PIN2_GPIO_PIN, PIOS_ADC_PIN3_GPIO_PIN }
-#define PIOS_ADC_CHANNELS			{ PIOS_ADC_PIN1_GPIO_CHANNEL, PIOS_ADC_PIN2_GPIO_CHANNEL, PIOS_ADC_PIN3_GPIO_CHANNEL }
-#define PIOS_ADC_MAPPING			{ PIOS_ADC_PIN1_ADC, PIOS_ADC_PIN2_ADC, PIOS_ADC_PIN3_ADC }
-#define PIOS_ADC_CHANNEL_MAPPING		{ PIOS_ADC_PIN1_ADC_NUMBER, PIOS_ADC_PIN2_ADC_NUMBER, PIOS_ADC_PIN3_ADC_NUMBER }
-#define PIOS_ADC_NUM_CHANNELS			(PIOS_ADC_NUM_PINS + PIOS_ADC_USE_TEMP_SENSOR) 
-#define PIOS_ADC_NUM_ADC_CHANNELS		2
-#define PIOS_ADC_USE_ADC2			1
-#define PIOS_ADC_CLOCK_FUNCTION			RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_ADC2, ENABLE)
-#define PIOS_ADC_ADCCLK				RCC_PCLK2_Div8
-/* RCC_PCLK2_Div2: ADC clock = PCLK2/2 */
-/* RCC_PCLK2_Div4: ADC clock = PCLK2/4 */
-/* RCC_PCLK2_Div6: ADC clock = PCLK2/6 */
-/* RCC_PCLK2_Div8: ADC clock = PCLK2/8 */
-#define PIOS_ADC_SAMPLE_TIME			ADC_SampleTime_239Cycles5
-/* Sample time: */
-/* With an ADCCLK = 14 MHz and a sampling time of 239.5 cycles: */
-/* Tconv = 239.5 + 12.5 = 252 cycles = 18�s */
-/* (1 / (ADCCLK / CYCLES)) = Sample Time (�S) */
-#define PIOS_ADC_IRQ_PRIO			PIOS_IRQ_PRIO_LOW
-
-// Currently analog acquistion hard coded at 480 Hz
-// PCKL2 = HCLK / 16
-// ADCCLK = PCLK2 / 2
-#define PIOS_ADC_RATE		(72.0e6 / 1.0 / 8.0 / 252.0 / (PIOS_ADC_NUM_CHANNELS >> PIOS_ADC_USE_ADC2))
-#define PIOS_ADC_MAX_OVERSAMPLING               36
 
 //-------------------------
 // Receiver PWM inputs
