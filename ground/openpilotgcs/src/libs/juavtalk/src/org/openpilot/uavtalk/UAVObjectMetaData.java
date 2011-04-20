4b7dbb5fcf9b452ba2012e826c891cc835827fb5
marcus bueschleb <ligi@ligi.de>
2011-04-20 03:57:42 +0200

    correct indent

--- ground/openpilotgcs/src/libs/juavtalk/src/org/openpilot/uavtalk/UAVObjectMetaData.java
+++ ground/openpilotgcs/src/libs/juavtalk/src/org/openpilot/uavtalk/UAVObjectMetaData.java
@@ -27,49 +27,49 @@ package org.openpilot.uavtalk;
 */
 public class UAVObjectMetaData {
 
-	public final static byte ACCESS_READWRITE=0;
-	public final static byte ACCESS_READONLY=1;
-	public final static byte ACCESS_WRITEONLY=2;
+    public final static byte ACCESS_READWRITE=0;
+    public final static byte ACCESS_READONLY=1;
+    public final static byte ACCESS_WRITEONLY=2;
 
-	public final static String getAccessString(byte access_mode) {
-		switch (access_mode) {
-		case ACCESS_READWRITE:
-			return "Read/Write";
-		case ACCESS_WRITEONLY:
-			return "Write only";
-		case ACCESS_READONLY:
-			return "Read only";
-		default:
-			return "unknowm Access Mode";
-		}
+    public final static String getAccessString(byte access_mode) {
+	switch (access_mode) {
+	case ACCESS_READWRITE:
+	    return "Read/Write";
+	case ACCESS_WRITEONLY:
+	    return "Write only";
+	case ACCESS_READONLY:
+	    return "Read only";
+	default:
+	    return "unknowm Access Mode";
 	}
+    }
 	
-	public final static byte UPDATEMODE_NEVER=0;
-	public final static byte UPDATEMODE_MANUAL=1;
-	public final static byte UPDATEMODE_ONCHANGE=2;
-	public final static byte UPDATEMODE_PERIODIC=3;
-	
-	public final static String getUpdateModeString(byte update_mode) {
-		switch(update_mode) {
-		case UPDATEMODE_NEVER:
-			return "never";
-		case UPDATEMODE_MANUAL:
-			return "manual";
-		case UPDATEMODE_ONCHANGE:
-			return "on change";
-		case UPDATEMODE_PERIODIC:
-			return "periodic";
-		default:
-			return "unknown";
-			
-		}
+    public final static byte UPDATEMODE_NEVER=0;
+    public final static byte UPDATEMODE_MANUAL=1;
+    public final static byte UPDATEMODE_ONCHANGE=2;
+    public final static byte UPDATEMODE_PERIODIC=3;
+    
+    public final static String getUpdateModeString(byte update_mode) {
+	switch(update_mode) {
+	case UPDATEMODE_NEVER:
+	    return "never";
+	case UPDATEMODE_MANUAL:
+	    return "manual";
+	case UPDATEMODE_ONCHANGE:
+	    return "on change";
+	case UPDATEMODE_PERIODIC:
+	    return "periodic";
+	default:
+	    return "unknown";
+	    
 	}
+    }
 	
-	public final static boolean TRUE=true;
-	public final static boolean FALSE=false;
-	
-	/* constant */
-	public byte gcsAccess;
+    public final static boolean TRUE=true;
+    public final static boolean FALSE=false;
+    
+    /* constant */
+    public byte gcsAccess;
     public boolean gcsTelemetryAcked;
     public byte gcsTelemetryUpdateMode;
     public int gcsTelemetryUpdatePeriod;
