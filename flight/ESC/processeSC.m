69ac06618cb46f3cfd98b767b3df23e86a3b3ec4
James Cotton <peabody124@gmail.com>
2011-06-09 17:02:29 -0500

    Add matlab function to process backbuffer. Not really the right place.

---
+++ flight/ESC/processeSC.m
@@ -0,0 +1,40 @@
+%dump binary memory dump.bin &back_buf[0] &back_buf[8095]
+
+
+fid = fopen('dump.bin');
+dat = fread(fid);
+fclose(fid)
+%dat = reshape(dat,2,[]);
+dat = double(typecast(uint8(dat(:)),'uint16'));
+
+sync = find(dat == 65535);
+
+state = dat(sync + 1);
+detected = dat(sync + 2);
+commutated = dat(sync + 3);
+dat(sync+1) = 65535;
+dat(sync+2) = 65535;
+dat(sync+3) = 65535;
+dat(sync(end):end) = 65535;
+
+bad = find(mod(diff(sync)-4,3) ~= 0);
+for i = 1:length(bad)
+    dat(sync(bad(i)):sync(bad(i)+1)) = 65535;
+end
+
+dat(dat == 65535) = [];
+
+dat = reshape(dat,3,[]);
+plot(1:size(dat,2),dat','.',sync/3,state*40,'k')
+
+sync = sync - 2*(0:length(sync)-1)';
+
+dev = double(dat) - 580;
+
+t = 1:size(dev,2);
+h(1) = subplot(211);
+plot(t,bsxfun(@minus,bsxfun(@plus,0:2000:4000,dev'),0),sync / 3, state * 100 - 1000, '.k',sync / 3, mod(detected,2) * 1000 - 2000,'.',sync / 3, commutated * 1000 - 2500,'.')
+h(2) = subplot(212);
+avg = mean(dat,1);
+plot(t,bsxfun(@plus,bsxfun(@minus,dat,avg),[0 1500 3000]'),'.');
+linkaxes(h,'x')
