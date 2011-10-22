80ea7f163bccf6a6843d2b711fb86a569d855947
Brian Webb <webbbn@gmail.com>
2011-10-22 08:30:34 -0700

    Checked in local mods to firmware-defs.mk

--- make/firmware-defs.mk
+++ make/firmware-defs.mk
@@ -116,7 +116,7 @@ FORCE:
 
 $(1).firmwareinfo.c: $(1) $(TOP)/make/templates/firmwareinfotemplate.c FORCE
 	@echo $(MSG_FWINFO) $$(call toprel, $$@)
-	$(V1) python $(TOP)/make/scripts/version-info.py \
+	python $(TOP)/make/scripts/version-info.py \
 		--path=$(TOP) \
 		--template=$(TOP)/make/templates/firmwareinfotemplate.c \
 		--outfile=$$@ \
@@ -201,7 +201,6 @@ endef
 # $(1) = Name of binary image to write
 # $(2) = Base of flash region to write/wipe
 # $(3) = Size of flash region to write/wipe
-# $(4) = OpenOCD configuration file to use
 define JTAG_TEMPLATE
 # ---------------------------------------------------------------------------
 # Options for OpenOCD flash-programming
@@ -214,7 +213,7 @@ OOCD_EXE ?= openocd
 OOCD_JTAG_SETUP  = -d0
 # interface and board/target settings (using the OOCD target-library here)
 OOCD_JTAG_SETUP += -s $(TOP)/flight/Project/OpenOCD
-OOCD_JTAG_SETUP += -f flyswatter.cfg -f $(4)
+OOCD_JTAG_SETUP += -f flyswatter.cfg -f stm32f1x.cfg
 
 # initialize
 OOCD_BOARD_RESET = -c init
@@ -229,8 +228,8 @@ program: $(1)
 	$(V1) $(OOCD_EXE) \
 		$$(OOCD_JTAG_SETUP) \
 		$$(OOCD_BOARD_RESET) \
-		-c "flash write_image erase $$< $(2) bin" \
-		-c "verify_image $$< $(2) bin" \
+		-c "flash write_image erase $(subst c:,,$(1)) $(2) bin" \
+		-c "verify_image $(subst c:,,$(1)) $(2) bin" \
 		-c "reset run" \
 		-c "shutdown"
 
@@ -244,4 +243,3 @@ wipe:
 		-c "reset run" \
 		-c "shutdown"
 endef
-
