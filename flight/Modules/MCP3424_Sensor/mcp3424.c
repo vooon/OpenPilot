1d0eb5862c80df303033096ab899e07a0c180a94
Scott <carboninfusion@gmail.com>
2011-10-30 23:36:30 -0700

    Fixed MCP3424Settings bug and added ignition battery voltage and current sensing.

--- flight/Modules/MCP3424_Sensor/mcp3424.c
+++ flight/Modules/MCP3424_Sensor/mcp3424.c
@@ -44,8 +44,9 @@
 // Private constants
 #define STACK_SIZE_BYTES 600
 #define TASK_PRIORITY (tskIDLE_PRIORITY+1)
-#define UPDATE_PERIOD 200
+#define UPDATE_PERIOD 500
 static double_t vPerC = 0.0000403; //volts per celcius for K-type thermocouple
+static double_t MCP3424_REFVOLTAGE = 2.048; //internal reference voltage for MCP3424 IC
 
 // Private types
 #define MCP9804_I2C_ADDRESS 0x1F //Cold junction temperature sensor
@@ -80,6 +81,7 @@ int32_t MCP3424Start()
 int32_t MCP3424Initialize()
 {
 	MCP3424sensorInitialize(); //Initialise the UAVObject used for transferring data to GCS
+	MCP3424SettingsInitialize();
 
 	// init down-sampling data
 	MCP3424_ds_temp1 = 0;
@@ -148,48 +150,48 @@ static bool Read_Cold_Junction_Temp(double_t* pColdTemp)
 
 static uint8_t getGain(uint8_t x) {
 	switch(x) {
-	case(MCP3424SETTINGS_CHANNEL1GAIN_1):
+	case(0):
 		return 1;
 		break;
-	case(MCP3424SETTINGS_CHANNEL1GAIN_2):
+	case(1):
 		return 2;
 		break;
-	case(MCP3424SETTINGS_CHANNEL1GAIN_4):
+	case(2):
 		return 4;
 		break;
-	case(MCP3424SETTINGS_CHANNEL1GAIN_8):
+	case(3):
 		return 8;
 		break;
 	default:
-		return 8;
+		return -1;
 	}
 }
 
 static uint8_t getResolution(uint8_t x) {
 	switch(x) {
-	case(MCP3424SETTINGS_CHANNEL1RESOLUTION_12):
+	case(0):
 		return 12;
 		break;
-	case(MCP3424SETTINGS_CHANNEL1RESOLUTION_14):
+	case(1):
 		return 14;
 		break;
-	case(MCP3424SETTINGS_CHANNEL1RESOLUTION_16):
+	case(2):
 		return 16;
 		break;
-	case(MCP3424SETTINGS_CHANNEL1RESOLUTION_18):
+	case(3):
 		return 18;
 		break;
 	default:
-		return 18;
+		return -1;
 	}
 }
 
 static bool MCP3424SetConfig(uint8_t channel, uint8_t* dataNumBytes, uint8_t* pResolution, uint8_t* pGain, uint8_t* pConfigByte) {
 
-	uint8_t gain = 8;
-	uint8_t resolution = 18;
-	uint8_t PGAgain = 3;
-	uint8_t sampleRate = 3;
+	uint8_t gain = 0;
+	uint8_t resolution = 0;
+	uint8_t PGAgain = 0;
+	uint8_t sampleRate = 0;
 
 	uint8_t conversionModeBit = 0; //1=Continuous, 0=One Shot
 	uint8_t channelBits = 0; //0 = Channel 1
@@ -208,69 +210,68 @@ static bool MCP3424SetConfig(uint8_t channel, uint8_t* dataNumBytes, uint8_t* pR
 		break;
 	case(2):
 		gain = getGain(mcp3424settings.Channel2Gain);
-		resolution = getResolution(mcp3424settings.Channel1Resolution);
+		resolution = getResolution(mcp3424settings.Channel2Resolution);
 		break;
 	case(3):
 		gain = getGain(mcp3424settings.Channel3Gain);
-		resolution = getResolution(mcp3424settings.Channel1Resolution);
+		resolution = getResolution(mcp3424settings.Channel3Resolution);
 		break;
 	case(4):
 		gain = getGain(mcp3424settings.Channel4Gain);
-		resolution = getResolution(mcp3424settings.Channel1Resolution);
+		resolution = getResolution(mcp3424settings.Channel4Resolution);
 		break;
 	default:
-		gain = 8;
-		resolution = 18;
+		gain = -1;
+		resolution = -1;
 	}
 
 	*pResolution = resolution;
+	*pGain = gain;
 
 	channelBits = channel - 1; //zero based
 
 	switch(gain) {
 	case('8'):
-		PGAgain = 3;
+		PGAgain = 0x03;
 		break;
 	case('4'):
-		PGAgain = 2;
+		PGAgain = 0x02;
 		break;
 	case('2'):
-		PGAgain = 1;
+		PGAgain = 0x01;
 		break;
 	case('1'):
-		PGAgain = 0;
+		PGAgain = 0x00;
 		break;
 	default:
-		PGAgain = 3;
+		PGAgain = 0x00;
 	}
-
-	*pGain = PGAgain;
 
 	switch(resolution) {
 	case(18):
-		sampleRate = 3; //3.75 sps (18 bits), 3 bytes of data
+		sampleRate = 0x03; //3.75 sps (18 bits), 3 bytes of data
 		break;
 	case(16):
-		sampleRate = 2; //2 bytes of data,
+		sampleRate = 0x02; //2 bytes of data,
 		break;
 	case(14):
-		sampleRate = 1; //2 bytes of data
+		sampleRate = 0x01; //2 bytes of data
 		break;
 	case(12):
-		sampleRate = 0; //240 SPS (12 bits), 2 bytes of data
+		sampleRate = 0x00; //240 SPS (12 bits), 2 bytes of data
 		break;
 	default:
-		sampleRate = 3;
+		sampleRate = 0x00;
 	}
 
 	uint8_t config = PGAgain;
 	config = config | (sampleRate << 2);
 	config = config | (conversionModeBit << 4);
-	config = config | (channelBits << 6);
+	config = config | (channelBits << 5);
 	config = config | (1 << 7); //write a 1 here to initiate a new conversion in One-shot mode
 	*pConfigByte = config;
 
-	//the resolution setting effects the number of data bytes returned during a read
+	//the resolution setting affects the number of data bytes returned during a read
 	if(resolution == 18)
 		*dataNumBytes = 3;
 	else
@@ -289,35 +290,66 @@ static bool MCP3424SetConfig(uint8_t channel, uint8_t* dataNumBytes, uint8_t* pR
 	return PIOS_I2C_Transfer(PIOS_I2C_MAIN_ADAPTER, txn_list_1, NELEMENTS(txn_list_1));
 }
 
-static void decipherI2Cresponse(uint8_t* bufferTemp, uint8_t* pBuffer, uint8_t* numDataBytes, uint8_t* resolution, uint32_t* counts){
+static void decipherI2Cresponse(uint8_t* pBufferTemp, uint8_t* pBuffer, uint8_t* pNumDataBytes, uint8_t* pResolution, uint32_t* pCounts){
 	int8_t sign = 0; //+ve
 
-	if(*numDataBytes == 3) {
-		memcpy(pBuffer, bufferTemp, 4);
-		sign = bufferTemp[0];
+	if((*pNumDataBytes) == 3) {
+		memcpy(pBuffer, pBufferTemp, 4);
 
+		pBuffer[0] = pBuffer[0] & 0x01; //remove the repeated MSB and sign bit
 	}
 	else { //numDataBytes == 2.
 		pBuffer[0] = 0;              //set upper data byte to zero
-		pBuffer[1] = bufferTemp[0];  //middle data byte
-		pBuffer[2] = bufferTemp[1];  //lower data byte
-		pBuffer[3] = bufferTemp[2];  //config byte
+		pBuffer[1] = pBufferTemp[0];  //middle data byte
+		pBuffer[2] = pBufferTemp[1];  //lower data byte
+		pBuffer[3] = pBufferTemp[2];  //config byte
 
-		sign = bufferTemp[1];
+		if((*pResolution) == 14)
+			pBuffer[1] = pBuffer[1] & 0x1F; //remove the repeated MSB and sign bit
+		else if(*pResolution == 12)
+			pBuffer[1] = pBuffer[1] & 0x07; //remove the repeated MSB and sign bit
 	}
 
-	//TODO: need to ignore additional bits in the middle data byte if 12 or 14 bit resolution is selected
-	//This will only work for 18 bit resolution
-	pBuffer[0] = pBuffer[0] & 0x01; //ignore the first 7 bits
+	sign = pBufferTemp[0];
 
-	*counts = ((pBuffer[0] << 16) | (pBuffer[1] << 8) | pBuffer[2]);
+	*pCounts = ((pBuffer[0] << 16) | (pBuffer[1] << 8) | pBuffer[2]);
 
 	//Convert to 2's complement
 	if(sign < 0) {
-		int32_t largestNumber = ((1 << (*resolution - 1)) - 1);
-		*counts = -largestNumber + *counts;
+		int32_t largestNumber = ((1 << (*pResolution - 1)) - 1);
+		*pCounts = *pCounts - largestNumber;
 	}
+
+	pBuffer[4] = *pCounts;
+	pBuffer[5] = *pResolution;
 }
+
+static int32_t GetDelay(uint8_t* pResolution)
+{
+	double_t d = 0;
+	switch(*pResolution) {
+	case(12):
+		d = 1000/240.0; //240 samples per second
+		break;
+	case(14):
+		d = 1000/60.0;  //60 sps
+		break;
+	case(16):
+		d = 1000/15.0;  //15 sps
+		break;
+	case(18):
+		d = 1000/3.75;  //3.75 sps
+		break;
+	default:
+		d = 500;
+	}
+	return (int32_t)d + 50; //Add extra 50ms time
+}
+
+/*
+ * Read CylinderHeadTemp using a K-type thermocouple connected to Channel 1 of the MCP3424 IC
+ * on the I2C bus
+ */
 
 static bool ReadCylinderHeadTemp(uint8_t* pBuffer, double_t* pTemperature, uint8_t* pConfigByte)
 {
@@ -327,16 +359,13 @@ static bool ReadCylinderHeadTemp(uint8_t* pBuffer, double_t* pTemperature, uint8
 	uint8_t resolution, gain = 0;
 	uint32_t counts = 0;
 
-	double_t refVoltage = 2.048; //internal reference voltage for MCP3424 IC
-
 	bool success = MCP3424SetConfig(channel, &numDataBytes, &resolution, &gain, pConfigByte);
 
 	if(!success)
 		return false;
 
 	//wait long enough for conversion to happen after setting config
-	//TODO: make this wait dependent on the sample rate determined by the ADC resolution setting
-	vTaskDelay(100 / portTICK_RATE_MS); //100ms
+	vTaskDelay(GetDelay(&resolution) / portTICK_RATE_MS);
 
 	const struct pios_i2c_txn txn_list_1[] = {
 		{
@@ -354,8 +383,8 @@ static bool ReadCylinderHeadTemp(uint8_t* pBuffer, double_t* pTemperature, uint8
 
 		//Assume K-type thermocouple is connected to channel 1
 		//do conversion here
-		double_t LSB = 2 * refVoltage / (1 << resolution);
-		*pTemperature = counts * LSB / vPerC / gain;
+		double_t LSB = 2 * MCP3424_REFVOLTAGE / (1 << resolution);
+		*pTemperature = (int32_t)counts * LSB / vPerC / gain;
 
 		return true;
 	}
@@ -363,15 +392,18 @@ static bool ReadCylinderHeadTemp(uint8_t* pBuffer, double_t* pTemperature, uint8
 		return false;
 }
 
-static bool ReadVoltage(uint8_t* pBuffer, double_t* pVoltage, uint8_t* pConfigByte)
+/*
+ * Read Exhaust Gas Temp using a K-type thermocouple connected to Channel 2 of the MCP3424 IC
+ * on the I2C bus
+ */
+
+static bool ReadExhaustGasTemp(uint8_t* pBuffer, double_t* pTemperature, uint8_t* pConfigByte)
 {
-	uint8_t channel = 3;
+	uint8_t channel = 2;
 	uint8_t numDataBytes = 3;
 	uint8_t bufferTemp[4] = {0};  //buffer to store return data from sensor
 	uint8_t resolution, gain = 0;
 	uint32_t counts = 0;
-
-	double_t refVoltage = 2.048; //internal reference voltage for MCP3424 IC
 
 	bool success = MCP3424SetConfig(channel, &numDataBytes, &resolution, &gain, pConfigByte);
 
@@ -379,8 +411,7 @@ static bool ReadVoltage(uint8_t* pBuffer, double_t* pVoltage, uint8_t* pConfigBy
 		return false;
 
 	//wait long enough for conversion to happen after setting config
-	//TODO: make this wait dependent on the sample rate determined by the ADC resolution setting
-	vTaskDelay(100 / portTICK_RATE_MS); //100ms
+	vTaskDelay(GetDelay(&resolution) / portTICK_RATE_MS);
 
 	const struct pios_i2c_txn txn_list_1[] = {
 		{
@@ -396,11 +427,10 @@ static bool ReadVoltage(uint8_t* pBuffer, double_t* pVoltage, uint8_t* pConfigBy
 
 		decipherI2Cresponse(bufferTemp, pBuffer, &numDataBytes, &resolution, &counts);
 
-		//Assume Attopilot voltage and current sensor is being used.
-		// Specifically, the full scale voltage is 51.8V = 3.3V
-		// Full scale current is 90A = 3.3V
-		double_t LSB = 2 * refVoltage / (1 << resolution);
-		*pVoltage = counts * LSB * 51.8 / 3.3 / gain;
+		//Assume K-type thermocouple is connected to channel 1
+		//do conversion here
+		double_t LSB = 2 * MCP3424_REFVOLTAGE / (1 << resolution);
+		*pTemperature = (int32_t)counts * LSB / vPerC / gain;
 
 		return true;
 	}
@@ -408,7 +438,11 @@ static bool ReadVoltage(uint8_t* pBuffer, double_t* pVoltage, uint8_t* pConfigBy
 		return false;
 }
 
-static bool ReadCurrent(uint8_t* pBuffer, double_t* pCurrent, uint8_t* pConfigByte)
+/*
+ * Read the ignition battery voltage via the AttoPilot voltage and current sensor board connected to Channel 3
+ * of the MCP3424 IC on the I2C bus
+ */
+static bool ReadIgnitionBatteryVoltage(uint8_t* pBuffer, double_t* pVoltage, uint8_t* pConfigByte, uint32_t* pRawCounts)
 {
 	uint8_t channel = 3;
 	uint8_t numDataBytes = 3;
@@ -416,7 +450,57 @@ static bool ReadCurrent(uint8_t* pBuffer, double_t* pCurrent, uint8_t* pConfigBy
 	uint8_t resolution, gain = 0;
 	uint32_t counts = 0;
 
-	double_t refVoltage = 2.048; //internal reference voltage for MCP3424 IC
+	bool success = MCP3424SetConfig(channel, &numDataBytes, &resolution, &gain, pConfigByte);
+
+	if(!success)
+		return false;
+
+	//wait long enough for conversion to happen after setting config
+	vTaskDelay(GetDelay(&resolution) / portTICK_RATE_MS);
+
+	const struct pios_i2c_txn txn_list_1[] = {
+		{
+		 .addr = MCP3424_I2C_ADDRESS,
+		 .rw = PIOS_I2C_TXN_READ,
+		 .len = 4, //Upper, Middle, Lower data bytes and config byte returned for 18 bit mode
+		 .buf = bufferTemp,
+		 },
+	};
+
+	//Read data bytes
+	if(PIOS_I2C_Transfer(PIOS_I2C_MAIN_ADAPTER, txn_list_1, NELEMENTS(txn_list_1))) {
+
+		decipherI2Cresponse(bufferTemp, pBuffer, &numDataBytes, &resolution, &counts);
+
+		pBuffer[6] = gain;
+
+		//Assume Attopilot voltage and current sensor is being used.
+		// Specifically, the full scale voltage is 51.8V = 3.3V
+		// Full scale current is 90A = 3.3V
+		double_t attoPilotVscale = 1 / 0.06369; //From data sheet
+
+		double_t LSB = 2 * MCP3424_REFVOLTAGE / (1 << resolution);
+		*pVoltage = (int32_t)counts * LSB / gain * attoPilotVscale;
+
+		*pRawCounts = counts;
+
+		return true;
+	}
+	else
+		return false;
+}
+
+/*
+ * Read the ignition battery current via the AttoPilot voltage and current sensor board connected to Channel 4
+ * of the MCP3424 IC on the I2C bus
+ */
+static bool ReadIgnitionBatteryCurrent(uint8_t* pBuffer, double_t* pCurrent, uint8_t* pConfigByte)
+{
+	uint8_t channel = 4;
+	uint8_t numDataBytes = 3;
+	uint8_t bufferTemp[4] = {0};  //buffer to store return data from sensor
+	uint8_t resolution, gain = 0;
+	uint32_t counts = 0;
 
 	bool success = MCP3424SetConfig(channel, &numDataBytes, &resolution, &gain, pConfigByte);
 
@@ -424,8 +508,7 @@ static bool ReadCurrent(uint8_t* pBuffer, double_t* pCurrent, uint8_t* pConfigBy
 		return false;
 
 	//wait long enough for conversion to happen after setting config
-	//TODO: make this wait dependent on the sample rate determined by the ADC resolution setting
-	vTaskDelay(100 / portTICK_RATE_MS); //100ms
+	vTaskDelay(GetDelay(&resolution) / portTICK_RATE_MS);
 
 	const struct pios_i2c_txn txn_list_1[] = {
 		{
@@ -442,10 +525,11 @@ static bool ReadCurrent(uint8_t* pBuffer, double_t* pCurrent, uint8_t* pConfigBy
 		decipherI2Cresponse(bufferTemp, pBuffer, &numDataBytes, &resolution, &counts);
 
 		//Assume 90A Attopilot voltage and current sensor is being used.
-		// Specifically, the full scale voltage is 51.8V = 3.3V
 		// Full scale current is 90A = 3.3V
-		double_t LSB = 2 * refVoltage / (1 << resolution);
-		*pCurrent = counts * LSB * 90 / 3.3 / gain;
+		double_t attoPilotIscale = 1 / 0.0366; //From data sheet
+
+		double_t LSB = 2 * MCP3424_REFVOLTAGE / (1 << resolution);
+		*pCurrent = (int32_t)counts * LSB / gain * attoPilotIscale;
 
 		return true;
 	}
@@ -465,8 +549,9 @@ static void MCP3424Task(void *parameters)
 {
 	bool bParamSet = false;
 
-	uint8_t buf[4] = {0};
+	uint8_t buf[8] = {0};
 	uint8_t configByte = 0;
+	uint32_t rawCounts = 0;
 
 	portTickType lastSysTime;
 
@@ -481,6 +566,8 @@ static void MCP3424Task(void *parameters)
 //	double_t chan1_ds_count = 0;
 
 	double_t coldTemp = 0;
+
+	portTickType energyTimeTickCount = 0; //delta time for calculating battery energy consumption
 
 	bool bMCP3424readSuccess = false;
 
@@ -497,12 +584,10 @@ static void MCP3424Task(void *parameters)
 		/*
 		 * Read channel 1
 		 */
-
-		//Read thermocouple connected to channel 1 of MCP3424 IC via I2C
 		bMCP3424readSuccess = ReadCylinderHeadTemp(buf, &cylinderHeadTemp, &configByte);
 
 		if(bMCP3424readSuccess && b2) {
-			PIOS_LED_On(LED2);
+			PIOS_LED_Off(LED2);
 
 			//down sample
 //			chan1_ds_temp += channel1;
@@ -517,17 +602,11 @@ static void MCP3424Task(void *parameters)
 
 			cylinderHeadTemp += coldTemp; //thermocouple reads temperature relative to cold junction
 
-			d1.buf0 = buf[0]; //data1
-			d1.buf1 = buf[1]; //data2
-			d1.buf2 = buf[2]; //data3
-			//d1.buf3 = chanOneRaw;
 			d1.CylinderHeadTemp = cylinderHeadTemp; //calculated temperature
 			d1.ColdJunction = coldTemp;
-			d1.buf4 = configByte; //buf[3]; //config register
-			d1.buf5 = 0;
 		}
 		else {
-			PIOS_LED_Off(LED2);
+			PIOS_LED_On(LED2);
 			d1.buf0 = 99;
 			d1.buf1 = 99;
 			d1.buf2 = 99;
@@ -540,28 +619,42 @@ static void MCP3424Task(void *parameters)
 		/*
 		 * Read channel 2
 		 */
-		exhaustGasTemp = 0.0;
-		//TODO: Read analog channel here
+		//Read thermocouple connected to channel 1 of MCP3424 IC via I2C
+		ReadExhaustGasTemp(buf, &exhaustGasTemp, &configByte);
+		exhaustGasTemp += coldTemp; //thermocouple reads temperature relative to cold junction
 		d1.ExhaustGasTemp = exhaustGasTemp;
 
 		/*
 		 * Read channel 3
 		 */
-		bMCP3424readSuccess = ReadVoltage(buf, &batteryVoltage, &configByte);
-		//TODO: Read analog channel here
+		ReadIgnitionBatteryVoltage(buf, &batteryVoltage, &configByte, &rawCounts);
 		d1.BatteryVoltage = batteryVoltage;
+//		d1.buf0 = buf[0]; //data1
+//		d1.buf1 = buf[1]; //data2
+//		d1.buf2 = buf[2]; //data3
+//		d1.buf3 = buf[3]; //data4
+		//d1.CylinderHeadTemp = cylinderHeadTemp; //calculated temperature
+		//d1.ColdJunction = coldTemp;
+//		d1.buf4 = buf[4]; //raw counts
+//		d1.buf5 = buf[5];
+//		d1.buf6 = buf[6];
+//		d1.buf6 = configByte;
+//		d1.buf7 = rawCounts;
 
 		/*
 		 * Read channel 4
 		 */
-		bMCP3424readSuccess = ReadCurrent(buf, &batteryCurrent, &configByte);
-		//TODO: Read analog channel here
+		ReadIgnitionBatteryCurrent(buf, &batteryCurrent, &configByte);
 		d1.BatteryAmps = batteryCurrent;
 
-		/*
-		 * TODO: Compute energy consumed
-		 */
-		//d1.BatteryEnergyConsumed = deltaT * batteryVoltage * batteryCurrent;
+		portTickType x = xTaskGetTickCount();
+		portTickType deltaT = (x - energyTimeTickCount) * portTICK_RATE_MS; //Conversion to milliseconds
+		energyTimeTickCount = x;
+
+		float_t mAh = 0.0;
+		MCP3424sensorIgnitionBattery_mAhGet(&mAh); //This allows some other module or the GCS to reset it to zero
+
+		d1.IgnitionBattery_mAh = mAh + (deltaT * batteryCurrent / 3600.0); //Conversion to mAh
 
 		/*
 		 * Update UAVObject data
