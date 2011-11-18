6b707fa2b019e490af376aa9078149ce7f5ceaeb
Scott <carboninfusion@gmail.com>
2011-11-17 22:45:04 -0800

    Created separate modules for generic analogsensors read from mcp3424 via I2C and gasengine sensors read from same.

---
+++ flight/Modules/GasEngineSensors/inc/gasenginesensors.h
@@ -0,0 +1,41 @@
+/**
+ ******************************************************************************
+ * @addtogroup OpenPilotModules OpenPilot Modules
+ * @{ 
+ * @addtogroup GasEngine Module
+ * @brief Communicate with MCP3424 and update @ref GasEngineData "GasEngineData UAV Object"
+ * @{ 
+ *
+ * @file       GasEngineSensors_H.h
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+ * @brief      mcp3424 module, reads 4 analog channels from mcp3424 via I2C
+ *
+ * @see        The GNU Public License (GPL) Version 3
+ *
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+#ifndef GasEngineSensors_H
+#define GasEngineSensors_H
+
+int32_t GasEngineSensorsInitialize();
+
+#endif // GasEngineSensors_H
+
+/**
+ * @}
+ * @}
+ */
