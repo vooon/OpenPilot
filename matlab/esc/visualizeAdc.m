6b0cb9d53c6403c80b3ef1cdeb7fef825a21eb6f
James Cotton <peabody124@gmail.com>
2011-12-04 01:36:42 -0600

    Got the ADC running much faster which reduces the effect of timing jitter on ADC values.  Updated the visualization code accordingly.

--- matlab/esc/visualizeAdc.m
+++ matlab/esc/visualizeAdc.m
@@ -1,11 +1,3 @@
-esc = enableSerialControl(esc);
-pause(0.1)
-esc = setSerialSpeed(esc, 0);
-pause(1.5)
-esc = setSerialSpeed(esc, 400)
-pause(0.05);
-[esc dat] = runAdcLog(esc);
-esc = setSerialSpeed(esc, 0);
 
 [esc dat] = runAdcLog(esc);
 
@@ -18,27 +10,16 @@ dat(f2) = dat(f2) - v2;
 f3 = dat >= v3;
 dat(f3) = dat(f3) - v3;
 
+scale = 3.3 / 1024 * 12.7 / 2.7;
+
 t = 1:size(dat,2);
 
 subplot(211)
-plot(t(f2(1,:)),dat(1,f3(1,:)), ...
-    t(f2(2,:)),dat(2,f2(2,:)), ...
-    t(f2(3,:)),dat(3,f2(2,:)));
+plot(t(f3(1,:)),dat(:,f3(1,:)) * scale);
+ylim([0 12]);
+title('Off period');
+
 subplot(212)
-plot(t(~f2(1,:)),dat(1,~f3(1,:)), ...
-    t(~f2(2,:)),dat(2,~f2(2,:)), ...
-    t(~f2(3,:)),dat(3,~f2(2,:)));
-
-high = [dat(1,~f3(1,:)); ...
-    dat(2,~f2(2,:)); ...
-    dat(3,~f2(2,:))];
-
-low = [dat(1,f3(1,:)); ...
-    dat(2,f2(2,:)); ...
-    dat(3,f2(2,:))];
-
-h(1) = subplot(211);
-plot(t(1:2:end),bsxfun(@minus,high',mean(high,1)'),t,zeros(size(t)),'k')
-h(2) = subplot(212);
-plot(t(1:2:end),bsxfun(@minus,low',mean(low,1)'),t,zeros(size(t)),'k')
-linkaxes(h,'x');
+plot(t(~f3(1,:)),dat(:,~f3(1,:))* scale);
+ylim([0 6]);
+title('On period');
