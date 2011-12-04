d78877f6495dab3695ba44579cec54781c443d92
James Cotton <peabody124@gmail.com>
2011-12-03 19:05:24 -0600

    Set the ADC to synchronize to the PWM outputs.  Really cleans up the traces.

--- flight/ESC/pios_board.c
+++ flight/ESC/pios_board.c
@@ -37,7 +37,7 @@ void PIOS_TIM4_irq_handler() {
 #include "pios_rcvr_priv.h"
 uint32_t pios_rcvr_group_map[1];
 
-#define ESC_DEFAULT_PWM_RATE 25000
+#define ESC_DEFAULT_PWM_RATE 45000
 
 #include "pios_esc_priv.h"
 const struct pios_esc_cfg pios_esc_cfg = {
@@ -46,7 +46,7 @@ const struct pios_esc_cfg pios_esc_cfg = {
 		// This is 72e6 or 24e6 / 10e6 to give a 0.1 us resolution
 		.TIM_Prescaler = (PIOS_MASTER_CLOCK / 72e6) - 1,
 		.TIM_ClockDivision = TIM_CKD_DIV1,
-		.TIM_CounterMode = TIM_CounterMode_Up,
+		.TIM_CounterMode = TIM_CounterMode_CenterAligned1,
 		.TIM_Period = ((72e6 / ESC_DEFAULT_PWM_RATE) - 1),
 		.TIM_RepetitionCounter = 0x0000,
 	},
