6608faae15908724d2f766f753a78ad9c1587b85
James Cotton <peabody124@gmail.com>
2012-03-01 18:19:45 -0600

    Check in code for packaging firmware and bootloader into one package

---
+++ matlab/esc/escbin.m
@@ -0,0 +1,11 @@
+bl_size = hex2dec('2000');
+fid = fopen('build/bl_esc/bl_esc.bin');
+dat = uint8(fread(fid,'uchar'));
+fclose(fid);
+dat(bl_size) = 0;  % pad till end
+fid = fopen('build/fw_esc/fw_esc.bin');
+dat = [dat; uint8(fread(fid,'uchar'))];
+fclose(fid);
+fid = fopen('esc.bin','w');
+fwrite(fid,dat)
+fclose(fid)
