4c9a4d413854d22a2a4c8e22c6e15f2fcfd51601
James Cotton <peabody124@gmail.com>
2012-03-22 17:34:10 -0500

    Use cross-platform friendly usleep

--- ground/openpilotgcs/src/plugins/esc/escgadgetwidget.cpp
+++ ground/openpilotgcs/src/plugins/esc/escgadgetwidget.cpp
@@ -39,6 +39,15 @@
 #define NO_PORT         0
 #define SERIAL_PORT     1
 
+class SleepThread : public QThread
+{
+public:
+    static void usleep(unsigned long usecs)
+    {
+        QThread::usleep(usecs);
+    }
+};
+
 // constructor
 EscGadgetWidget::EscGadgetWidget(QWidget *parent) :
     QWidget(parent),
@@ -461,7 +470,7 @@ void EscGadgetWidget::updateCode()
     escSerial->bootloader();
     disconnectPort();
 
-    usleep(550000);
+    SleepThread::usleep(550000);
     rescueCode();
 
     connectPort();
