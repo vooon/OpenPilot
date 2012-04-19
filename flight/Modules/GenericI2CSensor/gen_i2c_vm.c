67a6de3abbc1e559bce08c11196c74dfab4dff81
Stacey Sheldon <stac@solidgoldbomb.org>
2012-04-19 19:25:20 -0400

    i2c-vm: Add a virtual machine that can drive i2c bus transactions
    This virtual machine can be programmed by the GCS using a custom
    byte code.  The optional GenericI2CSensor module will load load
    the program from a settings object and will boot the I2C VM to
    run the user's program.
    
    Some of the registers from the VM are exported back to the GCS
    via the GenericI2CSensor UAVO whenever the user's program requests
    it.

---
+++ flight/Modules/GenericI2CSensor/gen_i2c_vm.c
@@ -0,0 +1,394 @@
+/**
+ ******************************************************************************
+ * @addtogroup OpenPilotModules OpenPilot Modules
+ * @{ 
+ * @addtogroup Generic I2C Virtual Machine Module
+ * @brief Generic Programmable I2C Virtual Machine Module
+ * @{ 
+ *
+ * @file       
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2012.
+ * @brief      Generic Programmable I2C Virtual Machine Module
+ *
+ * @see        The GNU Public License (GPL) Version 3
+ *
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include <pios.h>
+#include "generici2csensor.h"
+
+#define I2C_VM_RAM_SIZE 8
+
+struct i2c_vm_regs {
+	bool     halted;
+	bool     fault;
+	uint8_t  pc;
+	uint32_t ctr;
+
+	uint8_t i2c_dev_addr;
+	uint8_t ram[I2C_VM_RAM_SIZE];
+
+	GenericI2CSensorData uavo;
+
+	int32_t  r0;
+	int32_t  r1;
+	int32_t  r2;
+	int32_t  r3;
+	float    f0;
+	float    f1;
+	float    f2;
+	float    f3;
+};
+
+static bool i2c_vm_halt (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	vm_state->halted = true;
+	return (true);
+}
+
+static bool i2c_vm_nop (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	vm_state->pc++;
+	return (true);
+}
+
+static bool i2c_vm_set_ctr (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	vm_state->ctr = op1;
+	vm_state->pc++;
+	return (true);
+}
+
+static bool i2c_vm_store (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	if (op2 >= sizeof(vm_state->ram)) {
+		return (false);
+	}
+
+	vm_state->ram[op2] = op1;
+	vm_state->pc++;
+	return (true);
+}
+
+enum i2c_exported_regs {
+	VM_R0,
+	VM_R1,
+	VM_F0,
+};
+
+static bool i2c_vm_load (struct i2c_vm_regs * vm_state, uint32_t val, uint8_t op3)
+{
+	switch (op3) {
+	case VM_R0:
+		vm_state->r0 = val;
+		break;
+	case VM_R1:
+		vm_state->r1 = val;
+		break;
+	case VM_F0:
+		vm_state->f0 = val;
+		break;
+	default:
+		return (false);
+	}
+
+	vm_state->pc++;
+	return (true);
+}
+
+static bool i2c_vm_load_be (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	if ((op2 < 1) || (op2 > 4)) {
+		return (false);
+	}
+
+	uint32_t val;
+	memcpy (&val, &(vm_state->ram[op1]), op2);
+
+	/* Handle byte swapping */
+	val = (((val & 0xFF000000) >> 24) |
+		((val & 0x00FF0000) >> 8) |
+		((val & 0x0000FF00) << 8) |
+		((val & 0x000000FF) << 24));
+
+	return (i2c_vm_load (vm_state, val, op3));
+}
+
+static bool i2c_vm_load_le (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	if ((op2 < 1) || (op2 > 4)) {
+		return (false);
+	}
+
+	uint32_t val;
+	memcpy (&val, &(vm_state->ram[op1]), op2);
+
+	return (i2c_vm_load (vm_state, val, op3));
+}
+
+/* Counter decrements to zero */
+static bool i2c_vm_dec_ctr (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	if (vm_state->ctr > 0) {
+		vm_state->ctr--;
+	}
+	vm_state->pc++;
+	return (true);
+}
+
+static bool i2c_vm_jump (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	vm_state->pc += op1;
+	return (true);
+}
+
+static bool i2c_vm_bnz (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	if (vm_state->ctr) {
+		return (i2c_vm_jump(vm_state, op1, op2, op3));
+	} else {
+		vm_state->pc++;
+	}
+	return (true);
+}
+
+static bool i2c_vm_set_dev_addr (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	vm_state->i2c_dev_addr = op1;
+	vm_state->pc++;
+	return (true);
+}
+
+static bool i2c_vm_read (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	/* Make sure our read fits in our buffer */
+	if (op1 > sizeof(vm_state->ram)) {
+		return false;
+	}
+
+	const struct pios_i2c_txn txn_list[] = {
+		{
+			.info = __func__,
+			.addr = vm_state->i2c_dev_addr,
+			.rw = PIOS_I2C_TXN_READ,
+			.len = op1,
+			.buf = vm_state->ram,
+		},
+	};
+
+	vm_state->pc++;
+	return (PIOS_I2C_Transfer(PIOS_I2C_MAIN_ADAPTER, txn_list, NELEMENTS(txn_list)));
+}
+
+static bool i2c_vm_write (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	if (op1 > sizeof(vm_state->ram)) {
+		return (false);
+	}
+
+	const struct pios_i2c_txn txn_list[] = {
+		{
+			.info = __func__,
+			.addr = vm_state->i2c_dev_addr,
+			.rw = PIOS_I2C_TXN_WRITE,
+			.len = op1,
+			.buf = vm_state->ram,
+		},
+	};
+
+	vm_state->pc++;
+	return (PIOS_I2C_Transfer(PIOS_I2C_MAIN_ADAPTER, txn_list, NELEMENTS(txn_list)));
+}
+
+static bool i2c_vm_send_uavo (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	/* Copy our exportable state into the uavo */
+	vm_state->uavo.r0 = vm_state->r0;
+	vm_state->uavo.r1 = vm_state->r1;
+	vm_state->uavo.f0 = vm_state->f0;
+
+	GenericI2CSensorSet(&vm_state->uavo);
+
+	vm_state->pc++;
+	return (true);
+}
+
+static bool i2c_vm_delay (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3)
+{
+	vTaskDelay(op1);
+	vm_state->pc++;
+	return (true);
+}
+
+static bool i2c_vm_reboot (struct i2c_vm_regs * vm_state)
+{
+	vm_state->halted = false;
+	vm_state->fault  = false;
+	vm_state->pc     = 0;
+	vm_state->ctr    = 0;
+
+	vm_state->r0     = 0;
+	vm_state->r1     = 0;
+	vm_state->r2     = 0;
+	vm_state->r3     = 0;
+	vm_state->f0     = (float) 0.0;
+	vm_state->f1     = (float) 0.0;
+	vm_state->f2     = (float) 0.0;
+	vm_state->f3     = (float) 0.0;
+
+	vm_state->i2c_dev_addr = 0;
+	memset(vm_state->ram, 0, sizeof(vm_state->ram));
+
+	return true;
+}
+
+enum i2c_vm_opcodes {
+	I2C_VM_OP_HALT,
+	I2C_VM_OP_NOP,
+	I2C_VM_OP_STORE,
+	I2C_VM_OP_LOAD_BE,
+	I2C_VM_OP_LOAD_LE,
+	I2C_VM_OP_SET_CTR,
+	I2C_VM_OP_DEC_CTR,
+	I2C_VM_OP_BNZ,
+	I2C_VM_OP_JUMP,
+	I2C_VM_OP_SET_DEV_ADDR,
+	I2C_VM_OP_READ,
+	I2C_VM_OP_WRITE,
+	I2C_VM_OP_SEND_UAVO,
+	I2C_VM_OP_DELAY,
+};
+
+typedef bool (*i2c_vm_inst_handler) (struct i2c_vm_regs * vm_state, uint8_t op1, uint8_t op2, uint8_t op3);
+
+const i2c_vm_inst_handler i2c_vm_handlers[] = {
+	[I2C_VM_OP_HALT]         = i2c_vm_halt,
+	[I2C_VM_OP_NOP]          = i2c_vm_nop,
+	[I2C_VM_OP_STORE]        = i2c_vm_store,
+	[I2C_VM_OP_LOAD_BE]      = i2c_vm_load_be,
+	[I2C_VM_OP_LOAD_LE]      = i2c_vm_load_le,
+	[I2C_VM_OP_SET_CTR]      = i2c_vm_set_ctr,
+	[I2C_VM_OP_DEC_CTR]      = i2c_vm_dec_ctr,
+	[I2C_VM_OP_BNZ]          = i2c_vm_bnz,
+	[I2C_VM_OP_JUMP]         = i2c_vm_jump,
+	[I2C_VM_OP_SET_DEV_ADDR] = i2c_vm_set_dev_addr,
+	[I2C_VM_OP_READ]         = i2c_vm_read,
+	[I2C_VM_OP_WRITE]        = i2c_vm_write,
+	[I2C_VM_OP_SEND_UAVO]    = i2c_vm_send_uavo,
+	[I2C_VM_OP_DELAY]        = i2c_vm_delay,
+};
+
+static bool i2c_vm_run (uint32_t * code, uint8_t code_len)
+{
+	static struct i2c_vm_regs vm;
+
+	i2c_vm_reboot (&vm);
+
+	while (!vm.halted) {
+		if (vm.pc > code_len) {
+			vm.fault  = true;
+			vm.halted = true;
+			continue;
+		}
+		/* Fetch */
+		uint32_t instruction = code[vm.pc];
+
+		/* Decode */
+		uint8_t operator = (instruction & 0xFF000000) >> 24;
+		uint8_t op1      = (instruction & 0x00FF0000) >> 16;
+		uint8_t op2      = (instruction & 0x0000FF00) >>  8;
+		uint8_t op3      = (instruction & 0x000000FF);
+
+		if (operator > NELEMENTS(i2c_vm_handlers)) {
+			vm.fault = true;
+			vm.halted = true;
+			continue;
+		}
+		i2c_vm_inst_handler f = i2c_vm_handlers[operator];
+
+		/* Execute + Writeback */
+		if (!f || !f(&vm, op1, op2, op3)) {
+			vm.fault  = true;
+			vm.halted = true;
+			continue;
+		}
+	}
+
+	return (!vm.fault);
+}
+
+#define I2C_VM_ASM(operator, op1, op2, op3) (((operator & 0xFF) << 24) | \
+						((op1 & 0xFF) << 16) | \
+						((op2 & 0xFF) << 8) | \
+						((op3 & 0xFF)))
+
+#define I2C_VM_ASM_NOP() (I2C_VM_ASM(I2C_VM_OP_NOP, 0, 0, 0))
+#define I2C_VM_ASM_HALT() (I2C_VM_ASM(I2C_VM_OP_HALT, 0, 0, 0))
+#define I2C_VM_ASM_SET_DEV_ADDR(addr) (I2C_VM_ASM(I2C_VM_OP_SET_DEV_ADDR, (addr), 0, 0))
+#define I2C_VM_ASM_WRITE(length) (I2C_VM_ASM(I2C_VM_OP_WRITE, (length), 0, 0))
+#define I2C_VM_ASM_READ(length) (I2C_VM_ASM(I2C_VM_OP_READ, (length), 0, 0))
+#define I2C_VM_ASM_DELAY(ms) (I2C_VM_ASM(I2C_VM_OP_DELAY, (ms), 0, 0))
+#define I2C_VM_ASM_JUMP(rel_addr) (I2C_VM_ASM(I2C_VM_OP_JUMP, (rel_addr), 0, 0))
+#define I2C_VM_ASM_STORE(value, addr) (I2C_VM_ASM(I2C_VM_OP_STORE, (value), (addr), 0))
+#define I2C_VM_ASM_BNZ(rel_addr) (I2C_VM_ASM(I2C_VM_OP_BNZ, (rel_addr), 0, 0))
+#define I2C_VM_ASM_SET_CTR(ctr_val) (I2C_VM_ASM(I2C_VM_OP_SET_CTR, (ctr_val), 0, 0))
+#define I2C_VM_ASM_DEC_CTR() (I2C_VM_ASM(I2C_VM_OP_DEC_CTR, 0, 0, 0))
+
+#define I2C_VM_ASM_LOAD_BE(addr, length, dest_reg) (I2C_VM_ASM(I2C_VM_OP_LOAD_BE, (addr), (length), (dest_reg)))
+#define I2C_VM_ASM_LOAD_LE(addr, length, dest_reg) (I2C_VM_ASM(I2C_VM_OP_LOAD_LE, (addr), (length), (dest_reg)))
+
+#define I2C_VM_ASM_SEND_UAVO() (I2C_VM_ASM(I2C_VM_OP_SEND_UAVO, 0, 0, 0))
+
+void gen_i2c_vm_test (void)
+{
+	uint32_t program[] = {
+		I2C_VM_ASM_NOP(),
+
+		I2C_VM_ASM_SET_CTR(10),
+		I2C_VM_ASM_SET_DEV_ADDR(0x53),
+		I2C_VM_ASM_STORE(0x2D, 0),
+		I2C_VM_ASM_STORE(0x08, 1),
+		I2C_VM_ASM_WRITE(2),
+		I2C_VM_ASM_DEC_CTR(),
+		I2C_VM_ASM_BNZ(-4),
+
+		I2C_VM_ASM_SET_DEV_ADDR(0x53),
+		I2C_VM_ASM_STORE(0x32, 0),
+		I2C_VM_ASM_WRITE(1),
+		I2C_VM_ASM_DELAY(50),
+		I2C_VM_ASM_READ(6),
+		I2C_VM_ASM_LOAD_LE(0, 2, VM_R0),
+		I2C_VM_ASM_LOAD_LE(2, 2, VM_R1),
+
+		I2C_VM_ASM_SET_DEV_ADDR(0x27),
+		
+		I2C_VM_ASM_SEND_UAVO(),
+		I2C_VM_ASM_JUMP(-9),
+	};
+
+	if (i2c_vm_run(program, NELEMENTS(program))) {
+		/* Program ran to completion */
+	} else {
+		/* Code faulted at some point */
+	}
+}
+
+/**
+ * @}
+ * @}
+ */
--- flight/Modules/GenericI2CSensor/generici2csensormodule.c
+++ flight/Modules/GenericI2CSensor/generici2csensormodule.c
@@ -37,52 +37,37 @@
  */
 
 #include "openpilot.h"
-#include "generici2csensor.h"	// UAVobject that will be updated by the module
+#include "hwsettings.h"
+#include "generici2csensor.h"
+#include "generici2csensorsettings.h"
 #include "pios_i2c.h"
 
 // Private constants
-#define STACK_SIZE_BYTES 600
+#define STACK_SIZE_BYTES 370
 #define TASK_PRIORITY (tskIDLE_PRIORITY+1)
-#define UPDATE_PERIOD 500
-//static double_t vPerC = 0.0000403; //volts per celcius for K-type thermocouple
-//static double_t MCP3424_REFVOLTAGE = 2.048; //internal reference voltage for MCP3424 IC
-
-// Private types
-#define MCP9804_I2C_ADDRESS 0x1F //Cold junction temperature sensor
-#define MCP3424_I2C_ADDRESS 0x68 //Four channel ADC sensor
 
 // Private variables
 static xTaskHandle taskHandle;
-
-// down sampling variables
-#define MCP3424_ds_size    4
+static bool genericI2CSensorModuleEnabled;
 
 // Private functions
 static void GenericI2CSensorTask(void *parameters);
-
-enum I2CPASS_NAMES {
-	FIRST_PASS, LOOP_PASS, UAVOBJECT_UPDATE_PASS
-};
-
-enum I2CSTEP_NAMES {
-	WAIT_STATE_0, WAIT_STATE_1, WAIT_STATE_2, WAIT_STATE_3,
-	CONFIG_REG_0, CONFIG_REG_1, CONFIG_REG_2, CONFIG_REG_3,
-	WRITE_REG_0, WRITE_REG_1,
-	READ_REG_0, READ_REG_1,
-	UAVOBJECT_DATA_0, UAVOBJECT_DATA_1, UAVOBJECT_DATA_2, UAVOBJECT_DATA_3,
-	UAVOBJECT_UPDATE
-	
-};
 
 /**
 * Start the module, called on startup
 */
 int32_t GenericI2CSensorModuleStart()
 {
-	// Start main task
-	xTaskCreate(GenericI2CSensorTask, (signed char *)"GenericI2CSensor", STACK_SIZE_BYTES/4, NULL, TASK_PRIORITY, &taskHandle);
-	TaskMonitorAdd(TASKINFO_RUNNING_GENERICI2CSENSOR, taskHandle);
-	return 0;
+	if (genericI2CSensorModuleEnabled) {
+		GenericI2CSensorSettingsInitialize();
+		GenericI2CSensorInitialize();
+
+		// Start main task
+		xTaskCreate(GenericI2CSensorTask, (signed char *)"GenericI2CSensor", STACK_SIZE_BYTES/4, NULL, TASK_PRIORITY, &taskHandle);
+		TaskMonitorAdd(TASKINFO_RUNNING_GENERICI2CSENSOR, taskHandle);
+		return 0;
+	}
+	return -1;
 }
 
 /**
@@ -90,233 +75,29 @@ int32_t GenericI2CSensorModuleStart()
 */
 int32_t GenericI2CSensorModuleInitialize()
 {
-	GenericI2CSensorInitialize(); //Initialise the UAVObject used for transferring data to GCS
-
+#ifdef MODULE_GenericI2CSensorModule_BUILTIN
+	genericI2CSensorModuleEnabled = 1;
+#else
+	HwSettingsInitialize();
+	uint8_t optionalModules[HWSETTINGS_OPTIONALMODULES_NUMELEM];
+	HwSettingsOptionalModulesGet(optionalModules);
+	if (optionalModules[HWSETTINGS_OPTIONALMODULES_GENERICI2CSENSOR] == HWSETTINGS_OPTIONALMODULES_ENABLED) {
+		genericI2CSensorModuleEnabled = 1;
+	} else {
+		genericI2CSensorModuleEnabled = 0;
+	}
+#endif
 	return 0;
 }
 
 MODULE_INITCALL(GenericI2CSensorModuleInitialize, GenericI2CSensorModuleStart)
 
+extern void gen_i2c_vm_test(void);
 
-/*
- * Read arbitrary registers
- *
- * Returns true if successful and false if not.
- */
-static bool ReadGenericRegisters(uint8_t i2c_address, uint8_t numRegisters, uint8_t* pBuffer)
-{
-	const struct pios_i2c_txn txn_list_1[] = {
-		{
-			.addr = i2c_address,
-			.rw = PIOS_I2C_TXN_READ,
-			.len = numRegisters, 
-			.buf = pBuffer,
-		},
-	};
-	
-	//Read data bytes
-	if(PIOS_I2C_Transfer(PIOS_I2C_MAIN_ADAPTER, txn_list_1, NELEMENTS(txn_list_1))) {
-		return true;
-	}
-	else
-		return false;
-}
-
-/*
-*/
-static bool WriteGenericRegisters(uint8_t i2c_address, uint8_t numRegisters, uint8_t* pBuffer)
-{
-	
-	//Set GenericI2CSensor config register via i2c
-	const struct pios_i2c_txn txn_list_1[] = {
-		{
-			.addr = i2c_address,
-			.rw = PIOS_I2C_TXN_WRITE,
-			.len = numRegisters,
-			.buf = pBuffer,
-		},
-	};
-	
-
-	//Write data bytes
-	if(PIOS_I2C_Transfer(PIOS_I2C_MAIN_ADAPTER, txn_list_1, NELEMENTS(txn_list_1))) {		
-		return true;
-	}
-	else
-		return false;
-}
-
-uint8_t jumpTableIndex=0;
-static void advance_to_next_I2C_step(enum I2CSTEP_NAMES *p_i2cStep, enum I2CPASS_NAMES *p_pass, uint8_t *jumpTable) {
-	
-	if (jumpTable[jumpTableIndex]==0xFF) { //Special value which causes jump table to loop
-		jumpTableIndex=0;
-	}
-	else if (jumpTable[jumpTableIndex]==0xFE) { //Special value which causes jump table to change type of pass
-		*p_pass=jumpTable[++jumpTableIndex];
-		*p_i2cStep=jumpTable[++jumpTableIndex];
-	}
-	else {
-		*p_i2cStep=jumpTable[++jumpTableIndex];
-	}
-}
-
-
-/**
- * Module thread, should not return.
- */
 static void GenericI2CSensorTask(void *parameters)
 {
-
-	portTickType lastSysTime;
-
-	//UAVObject data structure
-	GenericI2CSensorData d1;
-
-	// Main task loop
-	lastSysTime = xTaskGetTickCount();
-
-
-	
-	//MOST OF THESE VALUES NEED TO BE ABSTRACT AND READ IN FROM FLASH
-	enum I2CSTEP_NAMES i2cStep=WAIT_STATE_0;
-	enum I2CPASS_NAMES pass=FIRST_PASS;
-	uint8_t tmpLongBuffer[32];  //buffer to store return data from sensor //THIS IS A FIXED SIZE, SINCE IT HAS TO FIT INTO THE UAVOBJECT
-	portTickType xDelay;
-	portTickType xLastWakeTime=xTaskGetTickCount();
-	
-	char I2C_ADDRESS=0;
-	int NUM_REGISTERS_WRITE_CONFIG_REG_0=0, NUM_REGISTERS_WRITE_CONFIG_REG_1=0, NUM_REGISTERS_WRITE_CONFIG_REG_2=0, NUM_REGISTERS_WRITE_CONFIG_REG_3=0;
-	int NUM_REGISTERS_WRITE_REG_0=0, NUM_REGISTERS_WRITE_REG_1=0;
-	int NUM_REGISTERS_READ_REG_0=0, NUM_REGISTERS_READ_REG_1=0;
-	int WAIT_DELAY_ms_0=0, WAIT_DELAY_ms_1=0, WAIT_DELAY_ms_2=0, WAIT_DELAY_ms_3=0;
-	uint32_t VALUE_WRITE_CONFIG_REG_0=0, VALUE_WRITE_CONFIG_REG_1=0, VALUE_WRITE_CONFIG_REG_2=0, VALUE_WRITE_CONFIG_REG_3=0;
-	uint8_t VALUE_WRITE_REG_0[4];
-	uint8_t VALUE_WRITE_REG_1[4];
-	
-	/*
-	 *Temporary values for SRF08 (BEGIN)
-	 * V V V V V V V V V V V V V V V V
-	 */
-	I2C_ADDRESS=0x70;
-	WAIT_DELAY_ms_0=65;
-	VALUE_WRITE_REG_0[0] = 0x00;
-	VALUE_WRITE_REG_0[1] = 0x52;
-	NUM_REGISTERS_WRITE_REG_0=2;
-	VALUE_WRITE_REG_1[0] = 0x02;
-	NUM_REGISTERS_WRITE_REG_1=1;
-	NUM_REGISTERS_READ_REG_0=2;
-
-	uint8_t I2C_JUMPTABLE[] = {
-	  0xFE,
-	  LOOP_PASS,
-	  WRITE_REG_0,
-	  WAIT_STATE_0,
-	  WRITE_REG_1,
-	  READ_REG_0,
-	  0xFE,
-	  UAVOBJECT_UPDATE_PASS,
-	  UAVOBJECT_DATA_0,
-	  UAVOBJECT_DATA_1,
-	  UAVOBJECT_UPDATE,
-	  0xFF,
-	};
-
-	/* ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ 
-	 *Temporary values for SRF08 (END)
-	 */
-	
-	while(1) {
-
-		//Generic I2C addressing
-		if (i2cStep == WAIT_STATE_0) {
-				// Block for WAIT_DELAY_ms_0 [ms].
-				xDelay = WAIT_DELAY_ms_0 / portTICK_RATE_MS;
-				vTaskDelayUntil(&xLastWakeTime, xDelay);
-		}
-		else if( i2cStep == WAIT_STATE_1 ){
-			// Block for WAIT_DELAY_ms_1 [ms].
-			xDelay = WAIT_DELAY_ms_1 / portTICK_RATE_MS;
-			vTaskDelayUntil(&xLastWakeTime, xDelay);
-		}
-		else if( i2cStep == WAIT_STATE_2 ){
-			// Block for WAIT_DELAY_ms_2 [ms].
-			xDelay = WAIT_DELAY_ms_2 / portTICK_RATE_MS;
-			vTaskDelayUntil(&xLastWakeTime, xDelay);
-		}
-		else if ( i2cStep == WAIT_STATE_3 ){
-			// Block for WAIT_DELAY_ms_3 [ms].
-			xDelay = WAIT_DELAY_ms_3 / portTICK_RATE_MS;
-			vTaskDelayUntil(&xLastWakeTime, xDelay);
-		}
-		else if (pass==FIRST_PASS){
-			if ( i2cStep == CONFIG_REG_0 ){
-				WriteGenericRegisters(I2C_ADDRESS, NUM_REGISTERS_WRITE_CONFIG_REG_0, (uint8_t *) &VALUE_WRITE_CONFIG_REG_0);
-			}
-			else if ( i2cStep == CONFIG_REG_1 ){
-				WriteGenericRegisters(I2C_ADDRESS, NUM_REGISTERS_WRITE_CONFIG_REG_1, (uint8_t *) &VALUE_WRITE_CONFIG_REG_1);
-			}
-			else if ( i2cStep == CONFIG_REG_2 ){
-				WriteGenericRegisters(I2C_ADDRESS, NUM_REGISTERS_WRITE_CONFIG_REG_2, (uint8_t *) &VALUE_WRITE_CONFIG_REG_2);
-			}
-			else if ( i2cStep == CONFIG_REG_3 ){
-				WriteGenericRegisters(I2C_ADDRESS, NUM_REGISTERS_WRITE_CONFIG_REG_3, (uint8_t *) &VALUE_WRITE_CONFIG_REG_3);
-			}
-			
-			xLastWakeTime=xTaskGetTickCount();
-			
-		}
-		else if (pass==LOOP_PASS){
-			if ( i2cStep == WRITE_REG_0 ){
-				WriteGenericRegisters(I2C_ADDRESS, NUM_REGISTERS_WRITE_REG_0, VALUE_WRITE_REG_0);
-			}
-			else if ( i2cStep == WRITE_REG_1 ){
-				WriteGenericRegisters(I2C_ADDRESS, NUM_REGISTERS_WRITE_REG_1, VALUE_WRITE_REG_1);
-			}
-			else if ( i2cStep == READ_REG_0 ){
-				ReadGenericRegisters(I2C_ADDRESS, NUM_REGISTERS_READ_REG_0, tmpLongBuffer);
-//				decipherGenericI2CSensorresponse((uint8_t *) tmpLongBuffer, 0, 0, 0, 0);
-			}
-			else if ( i2cStep == READ_REG_1 ){
-				ReadGenericRegisters(I2C_ADDRESS, NUM_REGISTERS_READ_REG_1, tmpLongBuffer);
-//				decipherGenericI2CSensorresponse((uint8_t *) tmpLongBuffer, 0, 0, 0, 0);
-			}
-			
-			xLastWakeTime=xTaskGetTickCount();
-
-		}
-		else if (pass==UAVOBJECT_UPDATE_PASS){
-			if (i2cStep == UAVOBJECT_DATA_0){
-				d1.SensorOutput1 = *tmpLongBuffer;
-				//memcpy(&(d1.SensorOutput1), tmpLongBuffer, 1);
-			}
-			else if (i2cStep == UAVOBJECT_DATA_1){
-				d1.SensorOutput2 = *(tmpLongBuffer+1);
-				//memcpy(&(d1.SensorOutput2), tmpLongBuffer+1, 1);
-			}
-			else if (i2cStep == UAVOBJECT_DATA_2){
-				memcpy(&(d1.SensorOutput3), tmpLongBuffer, 4);
-			}
-			else if (i2cStep == UAVOBJECT_DATA_3){
-				memcpy(&(d1.SensorOutput4), tmpLongBuffer, 4);
-			}
-			else if (i2cStep == UAVOBJECT_UPDATE){
-				/*
-				 * Update UAVObject data
-				 */
-				GenericI2CSensorSet(&d1);
-			}			
-			
-		}
-		
-		advance_to_next_I2C_step(&i2cStep, &pass, I2C_JUMPTABLE);
-		
-		
-
-	}
+	gen_i2c_vm_test();
 }
-
-
 
 /**
   * @}
