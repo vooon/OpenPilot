2ec0c2b0600711817417890bb335512cfc57554d
dwobdar <dwobdar@gmail.com>
2012-06-03 22:39:31 +0200

    Can set correct postion for uavobjects waypoints.

--- ground/openpilotgcs/src/plugins/opmap/pathplannercontroller.cpp
+++ ground/openpilotgcs/src/plugins/opmap/pathplannercontroller.cpp
@@ -1,18 +1,26 @@
 #include "pathplannercontroller.h"
 
+#include <math.h>
+
 #include <QDebug>
+#include <qmath.h>
 
 #include <waypoint.h>
 #include <waypointactive.h>
 
 #include "extensionsystem/pluginmanager.h"
+#include "uavobjectutilmanager.h"
 #include "uavobjectmanager.h"
 #include "uavobject.h"
 
 #include "opmapcontrol/opmapcontrol.h"
 
 
-PathPlannerController::PathPlannerController(QObject* parent) : QObject(parent), m_pluginManager(NULL), m_objectManager(NULL), m_waypoint(NULL)
+PathPlannerController::PathPlannerController(QObject* parent) :
+    QObject(parent),
+    m_pluginManager(NULL),
+    m_objectManager(NULL),
+    m_waypoint(NULL)
 {
     m_pluginManager = ExtensionSystem::PluginManager::instance();
     Q_ASSERT(m_pluginManager);
@@ -20,6 +28,8 @@ PathPlannerController::PathPlannerController(QObject* parent) : QObject(parent),
     Q_ASSERT(m_objectManager);
     m_waypoint = Waypoint::GetInstance(m_objectManager);
     Q_ASSERT(m_waypoint != NULL);
+    m_objUtilManager = m_pluginManager->getObject<UAVObjectUtilManager>();
+    Q_ASSERT(m_objUtilManager);
 }
 
 PathPlannerController::~PathPlannerController()
@@ -29,30 +39,13 @@ PathPlannerController::~PathPlannerController()
 
 void PathPlannerController::waypointAdded(mapcontrol::WayPointItem* wp)
 {
-    quint32 newInstId = m_objectManager->getNumInstances(m_waypoint->getObjID());
-
-    if (wp->Number() < newInstId)
-    {
-        --newInstId;
-        Q_ASSERT(newInstId == wp->Number());
-        Waypoint *obj = Waypoint::GetInstance(m_objectManager, wp->Number());
-        WaypointUtil::UpdateWaypoint(wp, obj);
-    }
-
-    else
-    {
-        Waypoint *obj = new Waypoint();
-        quint32 newInstId = m_objectManager->getNumInstances(m_waypoint->getObjID());
-        obj->initialize(newInstId, obj->getMetaObject());
-        m_objectManager->registerObject(obj);
-        WaypointUtil::UpdateWaypoint(wp, obj);
-    }
+    AddWaypoint(wp);
 }
 
 void PathPlannerController::waypointChanged(mapcontrol::WayPointItem* wp)
 {
     Waypoint *obj = Waypoint::GetInstance(m_objectManager, wp->Number());
-    WaypointUtil::UpdateWaypoint(wp, obj);
+    UpdateWaypoint(wp, obj);
 }
 
 void PathPlannerController::waypointDeleted(mapcontrol::WayPointItem* wp)
@@ -64,23 +57,143 @@ void PathPlannerController::waypointDeleted(mapcontrol::WayPointItem* wp)
     //WaypointUtil::DeleteWaypoint(wp, obj);
 }
 
+Waypoint* PathPlannerController::AddWaypoint(mapcontrol::WayPointItem* wp)
+{
+    Waypoint *obj(NULL);
+    quint32 newInstId = m_objectManager->getNumInstances(m_waypoint->getObjID());
+
+    if (wp->Number() < newInstId)
+    {
+        --newInstId;
+        Q_ASSERT(newInstId == wp->Number());
+        obj = Waypoint::GetInstance(m_objectManager, wp->Number());
+        UpdateWaypoint(wp, obj);
+    }
+
+    else
+    {
+        obj = new Waypoint();
+        quint32 newInstId = m_objectManager->getNumInstances(m_waypoint->getObjID());
+        obj->initialize(newInstId, obj->getMetaObject());
+        m_objectManager->registerObject(obj);
+        UpdateWaypoint(wp, obj);
+    }
+
+    return obj;
+}
+
+void PathPlannerController::UpdateWaypoint(mapcontrol::WayPointItem* src, Waypoint* dest)
+{
+    Q_ASSERT(src);
+    Q_ASSERT(dest);
+
+    double home[3];
+    bool isHomeSet = false;
+
+    m_objUtilManager->getHomeLocation(isHomeSet, home);
+
+    if (isHomeSet)
+    {
+        WaypointUtil::UpdateWaypoint(home, src, dest);
+    }
+
+}
+
+void PathPlannerController::setWaypoints(const QList<mapcontrol::WayPointItem*> &waypoints)
+{
+    // TODO: Should we save this?
+    QList<Waypoint*> uavwaypoints;
+
+    foreach(mapcontrol::WayPointItem* wp, waypoints)
+    {
+        Waypoint *uavWaypoint = AddWaypoint(wp);
+        uavwaypoints.append(uavWaypoint);
+    }
+}
+
+//QList<mapcontrol::WayPointItem*>& PathPlannerController::getWaypoints()
+//{
+//    QList<mapcontrol::WayPointItem*> waypoints;
+//
+//    quint32 numUavWaypoints = m_objectManager->getNumInstances(m_waypoint->getObjID());
+//    for(quint32 ix=0; ix < numUavWaypoints; ++ix)
+//    {
+//        Waypoint *obj = Waypoint::GetInstance(m_objectManager, ix);
+//        mapcontrol::WayPointItem* waypoint = new mapcontrol::WayPointItem()
+//    }
+//
+//}
+
 void WaypointUtil::DeleteWaypoint(mapcontrol::WayPointItem* src, Waypoint* dest)
 {
     Q_ASSERT(src);
     Q_ASSERT(dest);
 }
 
-void WaypointUtil::UpdateWaypoint(mapcontrol::WayPointItem* src, Waypoint* dest)
+void WaypointUtil::UpdateWaypoint(double home[3], mapcontrol::WayPointItem* src, Waypoint* dest)
 {
     Q_ASSERT(src);
     Q_ASSERT(dest);
     Waypoint::DataFields waypoint = dest->getData();
 
-    waypoint.Position[Waypoint::POSITION_NORTH] = src->Coord().Lat();
-    waypoint.Position[Waypoint::POSITION_EAST] = src->Coord().Lng();
-    waypoint.Position[Waypoint::POSITION_DOWN] = src->Altitude();
+    waypoint.Position[Waypoint::POSITION_NORTH] = WaypointUtil::latDist(src, home[0]);
+    waypoint.Position[Waypoint::POSITION_EAST] = WaypointUtil::lngDist(src, home[1]);
+    waypoint.Position[Waypoint::POSITION_DOWN] = home[2] - src->Altitude();
+
+    //customData data = static_cast<customData>(src->customData());
+    //waypoint.setVelocity(data.velocity);
+    //struct customData
+    //        {
+    //                float velocity;
+    //                int mode;
+    //                float mode_params[4];
+    //                int condition;
+    //                float condition_params[4];
+    //                int command;
+    //                int jumpdestination;
+    //                int errordestination;
 
     dest->setData(waypoint);
     dest->updated();
-    qDebug() << "Set data for instance " << dest->getInstID();
+}
+
+qreal WaypointUtil::latDist(mapcontrol::WayPointItem* a, double b, qreal metric)
+{
+    const qreal cmetric = metric ? metric : latMetric(a);
+    return (a->Coord().Lat() - b) * cmetric;
+}
+
+//qreal WaypointUtil::latDist(mapcontrol::WayPointItem* a, mapcontrol::WayPointItem* b, qreal metric)
+//{
+//    const qreal cmetric = metric ? metric : latMetric(a);
+//    return (a->Coord().Lat() - b->Coord().Lat()) * cmetric;
+//}
+
+qreal WaypointUtil::lngDist(mapcontrol::WayPointItem* a, double b, qreal metric)
+{
+    const qreal cmetric = metric ? metric : latMetric(a);
+    return (a->Coord().Lng() - b) * cmetric;
+}
+
+//qreal WaypointUtil::lngDist(mapcontrol::WayPointItem* a, mapcontrol::WayPointItem* b, qreal metric)
+//{
+//    const qreal cmetric = metric ? metric : latMetric(a);
+//    return (a->Coord().Lng() - b->Coord().Lng()) * cmetric;
+//}
+
+qreal WaypointUtil::latMetric(mapcontrol::WayPointItem* wp)
+{
+    const qreal torad = (qreal)M_PI / 180.0;
+    const qreal a2 = 2 * wp->Coord().Lat() * torad;
+    const qreal a4 = 4 * wp->Coord().Lat() * torad;
+    const qreal result = 111132.954 -(559.822 * qCos(a2)) + (1.175 * qCos(a4));
+    return result;
+}
+
+qreal WaypointUtil::lngMetric(mapcontrol::WayPointItem* wp)
+{
+    const qreal torad = (qreal)M_PI / 180.0;
+    const qreal a = wp->Coord().Lat() * torad;
+    const qreal result = torad * 6367449.0 * qCos(a);
+    return result;
 }
--- ground/openpilotgcs/src/plugins/opmap/pathplannercontroller.h
+++ ground/openpilotgcs/src/plugins/opmap/pathplannercontroller.h
@@ -2,8 +2,9 @@
 #define PATHPLANNERCONTROLLER_H
 
 #include <QObject>
+#include <qmath.h>
 #include <waypoint.h>
-//#include <waypointactive.h>
+#include <waypointactive.h>
 
 
 namespace ExtensionSystem
@@ -17,13 +18,8 @@ namespace mapcontrol
 }
 
 class UAVObjectManager;
+class UAVObjectUtilManager;
 
-
-namespace WaypointUtil
-{
-    void UpdateWaypoint(mapcontrol::WayPointItem* src, Waypoint* dest);
-    void DeleteWaypoint(mapcontrol::WayPointItem* src, Waypoint* dest);
-}
 
 class PathPlannerController : public QObject
 {
@@ -31,6 +27,12 @@ class PathPlannerController : public QObject
 public:
     PathPlannerController(QObject *parent = 0);
     ~PathPlannerController();
+
+    void setWaypoints(const QList<mapcontrol::WayPointItem*> &waypoints);
+    //QList<mapcontrol::WayPointItem*>& getWaypoints();
+
+    //void Save();
+    //QList<mapcontrol::WayPointItem*> Load();
 
 public slots:
     void waypointAdded(mapcontrol::WayPointItem* wp);
@@ -40,10 +42,28 @@ public slots:
 signals:
 
 private:
+    Waypoint* AddWaypoint(mapcontrol::WayPointItem* wp);
+    void UpdateWaypoint(mapcontrol::WayPointItem* src, Waypoint* dest);
+
     ExtensionSystem::PluginManager * m_pluginManager;
     UAVObjectManager * m_objectManager;
+    UAVObjectUtilManager* m_objUtilManager;
     Waypoint* m_waypoint;
+    double m_home[3];
 };
+
+
+namespace WaypointUtil
+{
+    void UpdateWaypoint(double home[3], mapcontrol::WayPointItem* src, Waypoint* dest);
+    void DeleteWaypoint(mapcontrol::WayPointItem* src, Waypoint* dest);
+    qreal latDist(mapcontrol::WayPointItem* a, double b, qreal metric=0.0);
+    //qreal latDist(mapcontrol::WayPointItem* a, mapcontrol::WayPointItem* b, qreal metric=0.0);
+    qreal lngDist(mapcontrol::WayPointItem* a, double b, qreal metric=0.0);
+    //qreal lngDist(mapcontrol::WayPointItem* a, mapcontrol::WayPointItem* b, qreal metric=0.0);
+    qreal latMetric(mapcontrol::WayPointItem* wp);
+    qreal lngMetric(mapcontrol::WayPointItem* wp);
+}
 
 
 #endif // PATHPLANNERCONTROLLER_H
