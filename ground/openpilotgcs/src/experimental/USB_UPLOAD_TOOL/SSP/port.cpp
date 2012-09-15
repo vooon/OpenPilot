91a9e39c6f8799eb59d4586bfa46d82250cd8739
James Cotton <peabody124@gmail.com>
2012-09-14 22:08:25 -0500

    Cleanup DFU comments/debug output

--- ground/openpilotgcs/src/experimental/USB_UPLOAD_TOOL/SSP/port.cpp
+++ ground/openpilotgcs/src/experimental/USB_UPLOAD_TOOL/SSP/port.cpp
@@ -1,5 +1,8 @@
 #include "port.h"
 
+//#define FLUSH_ECHO
+//#define DEBUG_ECHO
+
 int readBytes = 0;
 port::port(PortSettings settings,QString name):mstatus(port::closed)
 {
@@ -7,10 +10,7 @@ port::port(PortSettings settings,QString name):mstatus(port::closed)
     timer.start();
     sport = new QextSerialPort(name,settings, QextSerialPort::Polling);
     if(sport->open(QIODevice::ReadWrite|QIODevice::Unbuffered))
-    {
         mstatus=port::open;
-      //  sport->setDtr();
-    }
     else
         mstatus=port::error;
 }
@@ -20,49 +20,48 @@ port::portstatus port::status()
 }
 int16_t port::pfSerialRead(void)
 {
-    QMutexLocker lock(portMutex);
-/*
     if (inputBuffer.length() > 0) {
         int16_t c = inputBuffer.at(0);
         inputBuffer = inputBuffer.mid(1,inputBuffer.length() - 1);
-        qDebug() << "returning a buffer byte";
         return c;
     }
-*/
+
     char c[1];
     if(sport->bytesAvailable()) {
         sport->read(c,1);
+#if defined(DEBUG_ECHO)
         qDebug() << QString("Read %1").arg((uint8_t)c[0],1,16);
+#endif
     }
     else
         return -1;
-    //qDebug() << "Read " << (uint8_t) c[0];
+
     return (uint8_t)c[0];
 }
 
 void port::pfSerialWrite(uint8_t c)
 {
-    //QMutexLocker lock(portMutex);
-/*
+
+#if defined(FLUSH_ECHO)
     usleep(100);
     while(sport->bytesAvailable()) {
-        qDebug() << "Flushing byte";
         char c[1];
         sport->read(c,1);
         inputBuffer.append(c);
     }
-*/
+#endif
 
     usleep(5000);
-    char cc[1];
-    cc[0]=c;
-    sport->write(cc,1);
+    sport->write((char *) &c,1);
+
+#if defined(FLUSH_ECHO)
     uint8_t echo[1];
-    //sport->read((char*)echo,1);
-    //qDebug() << QString("wrote %1 ").arg(c) << QString(" and echoed %1").arg(echo[0]);
-    /*if (echo[0] != c) {
+    sport->read((char*)echo,1);
+    if (echo[0] != c) {
         qDebug() << "Unoh.  Probably excluding echoed data";
-    }*/
+    }
+#endif
+
 }
 
 uint32_t port::pfGetTime(void)
