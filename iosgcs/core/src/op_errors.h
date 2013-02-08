465c9e9ffabc4ec38e7e55ed7715a9ea6b5801b0
Voha <vovastarikh@gmail.com>
2013-02-08 21:48:50 +0200

    + start native core

---
+++ iosgcs/core/src/op_errors.h
@@ -0,0 +1,37 @@
+/**
+ ******************************************************************************
+ *
+ * @file       op_errors.h
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2013.
+ * @see        The GNU Public License (GPL) Version 3
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef OpenPilotGCS_osx_op_errors_h
+#define OpenPilotGCS_osx_op_errors_h
+
+enum OpenPilotErrors
+{
+    OP_OK                = 0,
+    OPERR_FAIL           = -1,
+    OPERR_INVALIDCALL    = -2,
+    OPERR_INVALIDDATA    = -3,
+    OPER_OUTOFMEMORY     = -4,
+    OPERR_NOTIMPLEMENTED = -5
+};
+
+#endif
---
+++ iosgcs/core/src/optypes.h
@@ -0,0 +1,36 @@
+/**
+ ******************************************************************************
+ *
+ * @file       optypes.h
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2012.
+ * @see        The GNU Public License (GPL) Version 3
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef OpenPilotGCS__optypes_h
+#define OpenPilotGCS__optypes_h
+
+typedef char               opint8;
+typedef unsigned char      opuint8;
+typedef short              opint16;
+typedef unsigned short     opuint16;
+typedef int                opint32;
+typedef unsigned int       opuint32;
+typedef long long          opint64;
+typedef unsigned long long opuint64;
+
+#endif
