becbbfe455dee6a59f16cfd3962769bbbc2a0f52
Oleg Semyonov <os@openpilot.org>
2013-03-17 16:59:29 +0200

    *.mk: fix double '/' in path names ($dir always returns it as a last char)
    Also minor formatting of all library.mk files.

--- flight/PiOS/Common/Libraries/CMSIS2/library.mk
+++ flight/PiOS/Common/Libraries/CMSIS2/library.mk
@@ -2,5 +2,5 @@
 # Rules to add CMSIS2 to a PiOS target
 #
 
-CMSIS2_DIR		:=	$(dir $(lastword $(MAKEFILE_LIST)))
-EXTRAINCDIRS		+=	$(CMSIS2_DIR)/Include
+CMSIS2_DIR	:= $(dir $(lastword $(MAKEFILE_LIST)))
+EXTRAINCDIRS	+= $(CMSIS2_DIR)Include
