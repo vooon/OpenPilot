d272ffd18b9526bd07f3218cc0721a0e2a5c7995
caustic <nigel@kostiuck.com>
2013-04-04 11:31:47 -0700

    Correctly assign VAR to their inetent for the loop

--- make/boards/ccuniversal/STM32F103_board-info.mk
+++ make/boards/ccuniversal/STM32F103_board-info.mk
@@ -50,10 +50,10 @@ CMVENVER            := UNKNOWN
 
 # Patches required, make silliness requires funny numbering
 NUMPATCHES := 1 2
-PATCH_1    := ../../ExtLibraries/Patches/hw_config.h
-TARGET_1  := ../../ExtLibraries/Patches/STM32F10x_USB.diff
-PATCH_2    := ../../ExtLibraries/STM32_USB-FS-Device_Lib_V4.0.0/Libraries/STM32_USB-FS-Device_Driver/inc/usb_lib.h
-TARGET_2  := ../../ExtLibraries/Patches/STM32F10x_USB-2.diff
+TARGET_1    := ../../ExtLibraries/Patches/hw_config.h
+PATCH_1  := ../../ExtLibraries/Patches/STM32F10x_USB.diff
+TARGET_2    := ../../ExtLibraries/STM32_USB-FS-Device_Lib_V4.0.0/Libraries/STM32_USB-FS-Device_Driver/inc/usb_lib.h
+PATCH_2  := ../../ExtLibraries/Patches/STM32F10x_USB-2.diff
 
 OPENOCD_JTAG_CONFIG := foss-jtag.revb.cfg
 OPENOCD_CONFIG      := stm32f1x.cfg
