9d0f2f6e1710d0a27f84419bf9827797a5bdd285
caustic <nigel@kostiuck.com>
2013-04-17 21:49:17 -0700

    Remapped Perif clocks, AF is discarded as it is now a pin param

--- flight/PiOS/STM32F30x/pios_sys.c
+++ flight/PiOS/STM32F30x/pios_sys.c
@@ -52,8 +52,7 @@ void PIOS_SYS_Init(void)
 	SystemInit();
 
 	/* Enable GPIOA, GPIOB, GPIOC, GPIOD, GPIOE and AFIO clocks */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE |
-			       RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB | RCC_AHBPeriph_GPIOC | RCC_AHBPeriph_GPIOD | RCC_AHBPeriph_GPIOE, ENABLE);
 
 #if (PIOS_USB_ENABLED)
 	/*  Ensure that pull-up is active on detect pin */
