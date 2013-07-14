1c79269d3170a38f6f0f104494d372cf06fc0d59
Brian Webb <webbbn@gmail.com>
2013-07-14 08:47:16 -0700

    Splits out SPI ad I2C interface functions into separate files.

--- flight/pios/common/pios_mpu6000.c
+++ flight/pios/common/pios_mpu6000.c
@@ -34,8 +34,6 @@
 
 #ifdef PIOS_INCLUDE_MPU6000
 
-#include "fifo_buffer.h"
-
 enum pios_mpu6000_dev_magic {
     PIOS_MPU6000_DEV_MAGIC = 0x9da9b3ed,
 };
@@ -56,14 +54,17 @@ struct mpu6000_dev {
 static struct mpu6000_dev *dev;
 volatile bool mpu6000_configured = false;
 static uint32_t mpu6000_irq = 0;
-static uint8_t mpu6000_rec_buf[sizeof(struct pios_mpu6000_data) + 1];
 
 /* Private functions */
 static struct mpu6000_dev *PIOS_MPU6000_alloc(void);
 static int32_t PIOS_MPU6000_Validate(struct mpu6000_dev *dev);
 static void PIOS_MPU6000_Config(struct pios_mpu6000_cfg const *cfg);
-static int32_t PIOS_MPU6000_SetReg(uint8_t address, uint8_t buffer);
-static int32_t PIOS_MPU6000_GetReg(uint8_t address);
+
+/* External interface fuctions */
+extern void PIOS_MPU6000_SetClockSpeed(uint32_t interface_id, SPIPrescalerTypeDef speed);
+extern int32_t PIOS_MPU6000_Read(uint32_t interface_id, uint8_t slave_num, uint8_t addr, uint8_t len, uint8_t *data);
+extern int32_t PIOS_MPU6000_GetReg(uint32_t interface_id, uint8_t slave_num, uint8_t reg);
+extern int32_t PIOS_MPU6000_SetReg(uint32_t interface_id, uint8_t slave_num, uint8_t reg, uint8_t data);
 
 #define GRAV 9.81f
 
@@ -112,18 +113,6 @@ static int32_t PIOS_MPU6000_Validate(struct mpu6000_dev *vdev)
 }
 
 /**
- * @brief Change the interface clock speed
- */
-#ifdef PIOS_MPU6000_I2C_INTERFACE
-void PIOS_MPU6000_SetClockSpeed(__attribute__((unused)) uint32_t interface_id, __attribute__((unused)) SPIPrescalerTypeDef speed) {}
-#else
-void PIOS_MPU6000_SetClockSpeed(uint32_t interface_id, SPIPrescalerTypeDef speed)
-{
-    PIOS_SPI_SetClockSpeed(interface_id, speed);
-}
-#endif
-
-/**
  * @brief Initialize the MPU6000 3-axis gyro sensor.
  * @return 0 for success, -1 for failure
  */
@@ -159,13 +148,13 @@ static void PIOS_MPU6000_Config(struct pios_mpu6000_cfg const *cfg)
     PIOS_MPU6000_Test();
 
     // Reset chip
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_PWR_MGMT_REG, PIOS_MPU6000_PWRMGMT_IMU_RST) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_PWR_MGMT_REG, PIOS_MPU6000_PWRMGMT_IMU_RST) != 0) {
         ;
     }
     PIOS_DELAY_WaitmS(50);
 
     // Reset chip and fifo
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_USER_CTRL_REG,
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_USER_CTRL_REG,
                                PIOS_MPU6000_USERCTL_GYRO_RST |
                                PIOS_MPU6000_USERCTL_SIG_COND |
                                PIOS_MPU6000_USERCTL_FIFO_RST) != 0) {
@@ -173,7 +162,7 @@ static void PIOS_MPU6000_Config(struct pios_mpu6000_cfg const *cfg)
     }
 
     // Wait for reset to finish
-    while (PIOS_MPU6000_GetReg(PIOS_MPU6000_USER_CTRL_REG) &
+    while (PIOS_MPU6000_GetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_USER_CTRL_REG) &
            (PIOS_MPU6000_USERCTL_GYRO_RST |
             PIOS_MPU6000_USERCTL_SIG_COND |
             PIOS_MPU6000_USERCTL_FIFO_RST)) {
@@ -181,51 +170,51 @@ static void PIOS_MPU6000_Config(struct pios_mpu6000_cfg const *cfg)
     }
     PIOS_DELAY_WaitmS(10);
     // Power management configuration
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_PWR_MGMT_REG, cfg->Pwr_mgmt_clk) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_PWR_MGMT_REG, cfg->Pwr_mgmt_clk) != 0) {
         ;
     }
 
     // Interrupt configuration
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_INT_CFG_REG, cfg->interrupt_cfg) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_INT_CFG_REG, cfg->interrupt_cfg) != 0) {
         ;
     }
 
     // Interrupt configuration
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_INT_EN_REG, cfg->interrupt_en) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_INT_EN_REG, cfg->interrupt_en) != 0) {
         ;
     }
 
     // FIFO storage
 #if defined(PIOS_MPU6000_ACCEL)
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_FIFO_EN_REG, cfg->Fifo_store | PIOS_MPU6000_ACCEL_OUT) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_FIFO_EN_REG, cfg->Fifo_store | PIOS_MPU6000_ACCEL_OUT) != 0) {
         ;
     }
 #else
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_FIFO_EN_REG, cfg->Fifo_store) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_FIFO_EN_REG, cfg->Fifo_store) != 0) {
         ;
     }
 #endif
     PIOS_MPU6000_ConfigureRanges(cfg->gyro_range, cfg->accel_range, cfg->filter);
     // Interrupt configuration
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_USER_CTRL_REG, cfg->User_ctl) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_USER_CTRL_REG, cfg->User_ctl) != 0) {
         ;
     }
 
     // Interrupt configuration
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_PWR_MGMT_REG, cfg->Pwr_mgmt_clk) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_PWR_MGMT_REG, cfg->Pwr_mgmt_clk) != 0) {
         ;
     }
 
     // Interrupt configuration
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_INT_CFG_REG, cfg->interrupt_cfg) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_INT_CFG_REG, cfg->interrupt_cfg) != 0) {
         ;
     }
 
     // Interrupt configuration
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_INT_EN_REG, cfg->interrupt_en) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_INT_EN_REG, cfg->interrupt_en) != 0) {
         ;
     }
-    if ((PIOS_MPU6000_GetReg(PIOS_MPU6000_INT_EN_REG)) != cfg->interrupt_en) {
+    if ((PIOS_MPU6000_GetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_INT_EN_REG)) != cfg->interrupt_en) {
         return;
     }
 
@@ -250,12 +239,12 @@ int32_t PIOS_MPU6000_ConfigureRanges(
     PIOS_MPU6000_SetClockSpeed(dev->interface_id, PIOS_SPI_PRESCALER_256);
 
     // update filter settings
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_DLPF_CFG_REG, filterSetting) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_DLPF_CFG_REG, filterSetting) != 0) {
         ;
     }
 
     // Sample rate divider, chosen upon digital filtering settings
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_SMPLRT_DIV_REG,
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_SMPLRT_DIV_REG,
                                filterSetting == PIOS_MPU6000_LOWPASS_256_HZ ?
                                dev->cfg->Smpl_rate_div_no_dlp : dev->cfg->Smpl_rate_div_dlp) != 0) {
         ;
@@ -264,14 +253,14 @@ int32_t PIOS_MPU6000_ConfigureRanges(
     dev->filter = filterSetting;
 
     // Gyro range
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_GYRO_CFG_REG, gyroRange) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_GYRO_CFG_REG, gyroRange) != 0) {
         ;
     }
 
     dev->gyro_range = gyroRange;
 #if defined(PIOS_MPU6000_ACCEL)
     // Set the accel range
-    while (PIOS_MPU6000_SetReg(PIOS_MPU6000_ACCEL_CFG_REG, accelRange) != 0) {
+    while (PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_ACCEL_CFG_REG, accelRange) != 0) {
         ;
     }
 
@@ -281,162 +270,13 @@ int32_t PIOS_MPU6000_ConfigureRanges(
     return 0;
 }
 
-#ifdef PIOS_MPU6000_I2C_INTERFACE
-/**
- * @brief Reads one or more bytes into a buffer
- * \param[in] address HMC5883 register address (depends on size)
- * \param[out] buffer destination buffer
- * \param[in] len number of bytes which should be read
- * \return 0 if operation was successful
- * \return -1 if error during I2C transfer
- * \return -2 if unable to claim i2c device
- */
-static int32_t PIOS_MPU6000_I2CRead(uint8_t address, uint8_t *buffer, uint8_t len)
-{
-    uint8_t addr_buffer[] = {
-        address,
-    };
-
-    const struct pios_i2c_txn txn_list[] = {
-        {
-            .info = __func__,
-            .addr = dev->slave_num,
-            .rw   = PIOS_I2C_TXN_WRITE,
-            .len  = sizeof(addr_buffer),
-            .buf  = addr_buffer,
-        },
-        {
-            .info = __func__,
-            .addr = dev->slave_num,
-            .rw   = PIOS_I2C_TXN_READ,
-            .len  = len,
-            .buf  = buffer,
-        }
-    };
-
-    return PIOS_I2C_Transfer(dev->interface_id, txn_list, NELEMENTS(txn_list));
-}
-
-#else /* PIOS_MPU6000_I2C_INTERFACE */
-
-/**
- * @brief Claim the SPI bus for the accel communications and select this chip
- * @return 0 if successful, -1 for invalid device, -2 if unable to claim bus
- */
-static int32_t PIOS_MPU6000_ClaimBus()
-{
-    if (PIOS_MPU6000_Validate(dev) != 0) {
-        return -1;
-    }
-    if (PIOS_SPI_ClaimBus(dev->interface_id) != 0) {
-        return -2;
-    }
-    PIOS_SPI_RC_PinSet(dev->interface_id, dev->slave_num, 0);
-    return 0;
-}
-
-/**
- * @brief Release the SPI bus for the accel communications and end the transaction
- * @return 0 if successful
- */
-static int32_t PIOS_MPU6000_ReleaseBus()
-{
-    if (PIOS_MPU6000_Validate(dev) != 0) {
-        return -1;
-    }
-    PIOS_SPI_RC_PinSet(dev->interface_id, dev->slave_num, 1);
-    return PIOS_SPI_ReleaseBus(dev->interface_id);
-}
-#endif /* PIOS_MPU6000_I2C_INTERFACE */
-
-/**
- * @brief Read a register from MPU6000
- * @returns The register value or -1 if failure to get bus
- * @param reg[in] Register address to be read
- */
-static int32_t PIOS_MPU6000_GetReg(uint8_t reg)
-{
-    uint8_t data;
-
-#ifdef PIOS_MPU6000_I2C_INTERFACE
-    int32_t retval = PIOS_MPU6000_I2CRead(reg, &data, sizeof(data));
-
-    if (retval != 0) {
-        return retval;
-    } else {
-        return data;
-    }
-#else
-    if (PIOS_MPU6000_ClaimBus() != 0) {
-        return -1;
-    }
-
-    PIOS_SPI_TransferByte(dev->interface_id, (0x80 | reg)); // request byte
-    data = PIOS_SPI_TransferByte(dev->interface_id, 0); // receive response
-
-    PIOS_MPU6000_ReleaseBus();
-    return data;
-
-#endif /* PIOS_MPU6000_I2C_INTERFACE */
-}
-
-/**
- * @brief Writes one byte to the MPU6000
- * \param[in] reg Register address
- * \param[in] data Byte to write
- * \return 0 if operation was successful
- * \return -1 if unable to claim SPI bus
- * \return -2 if unable to claim i2c device
- */
-static int32_t PIOS_MPU6000_SetReg(uint8_t reg, uint8_t data)
-{
-#ifdef PIOS_MPU6000_I2C_INTERFACE
-    uint8_t i2c_data[] = {
-        reg,
-        data,
-    };
-
-    const struct pios_i2c_txn txn_list[] = {
-        {
-            .info = __func__,
-            .addr = dev->slave_num,
-            .rw   = PIOS_I2C_TXN_WRITE,
-            .len  = sizeof(i2c_data),
-            .buf  = i2c_data,
-        },
-    };
-
-    return PIOS_I2C_Transfer(dev->interface_id, txn_list, NELEMENTS(txn_list));
-
-#else
-    if (PIOS_MPU6000_ClaimBus() != 0) {
-        return -1;
-    }
-
-    if (PIOS_SPI_TransferByte(dev->interface_id, 0x7f & reg) != 0) {
-        PIOS_MPU6000_ReleaseBus();
-        return -2;
-    }
-
-    if (PIOS_SPI_TransferByte(dev->interface_id, data) != 0) {
-        PIOS_MPU6000_ReleaseBus();
-        return -3;
-    }
-
-    PIOS_MPU6000_ReleaseBus();
-
-    return 0;
-
-#endif /* ifdef PIOS_MPU6000_I2C_INTERFACE */
-}
-
 /*
  * @brief Read the identification bytes from the MPU6000 sensor
  * \return ID read from MPU6000 or -1 if failure
  */
 int32_t PIOS_MPU6000_ReadID()
 {
-    int32_t mpu6000_id = PIOS_MPU6000_GetReg(PIOS_MPU6000_WHOAMI);
+    int32_t mpu6000_id = PIOS_MPU6000_GetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_WHOAMI);
 
     if (mpu6000_id < 0) {
         return -1;
@@ -521,28 +361,13 @@ int32_t PIOS_MPU6000_Test(void)
  */
 static uint16_t PIOS_MPU6000_FifoDepth()
 {
-    uint8_t buf[3];
+    uint8_t buf[2] = { 0, 0 };
+    uint8_t len    = PIOS_MPU6000_Read(dev->interface_id, dev->slave_num, PIOS_MPU6000_FIFO_CNT_MSB, 2, buf);
 
-#ifdef PIOS_MPU6000_I2C_INTERFACE
-    if (PIOS_MPU6000_I2CRead(PIOS_MPU6000_FIFO_CNT_MSB, buf + 1, 2) < 0) {
-        return -1;
+    if (len < 2) {
+        return 0;
     }
-#else
-
-    if (PIOS_MPU6000_ClaimBus() != 0) {
-        return -1;
-    }
-
-    uint8_t mpu6000_send_buf[3] = { PIOS_MPU6000_FIFO_CNT_MSB | 0x80, 0, 0 };
-    if (PIOS_SPI_TransferBlock(dev->interface_id, &mpu6000_send_buf[0], &buf[0], sizeof(mpu6000_send_buf), NULL) < 0) {
-        PIOS_MPU6000_ReleaseBus();
-        return -1;
-    }
-
-    PIOS_MPU6000_ReleaseBus();
-#endif
-
-    return (buf[1] << 8) | buf[2];
+    return (buf[0] << 8) | buf[1];
 }
 
 /**
@@ -554,7 +379,7 @@ static uint8_t PIOS_MPU6000_GetStatus(void)
 {
     int32_t reg;
 
-    while ((reg = PIOS_MPU6000_GetReg(PIOS_MPU6000_INT_STATUS_REG)) < 0) {
+    while ((reg = PIOS_MPU6000_GetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_INT_STATUS_REG)) < 0) {
         ;
     }
     return reg;
@@ -576,9 +401,9 @@ static bool PIOS_MPU6000_Overflowed()
  */
 static void PIOS_MPU6000_FifoReset()
 {
-    uint8_t reg = PIOS_MPU6000_GetReg(PIOS_MPU6000_USER_CTRL_REG);
+    uint8_t reg = PIOS_MPU6000_GetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_USER_CTRL_REG);
 
-    PIOS_MPU6000_SetReg(PIOS_MPU6000_USER_CTRL_REG, reg | PIOS_MPU6000_USERCTL_FIFO_RST);
+    PIOS_MPU6000_SetReg(dev->interface_id, dev->slave_num, PIOS_MPU6000_USER_CTRL_REG, reg | PIOS_MPU6000_USERCTL_FIFO_RST);
 }
 
 /**
@@ -671,27 +496,13 @@ static void PIOS_MPU6000_Rotate(struct pios_mpu6000_data *data, const uint8_t *b
  */
 bool PIOS_MPU6000_ReadSensors(struct pios_mpu6000_data *data)
 {
-#ifdef PIOS_MPU6000_I2C_INTERFACE
-    if (PIOS_MPU6000_I2CRead(PIOS_MPU6000_ACCEL_X_OUT_MSB, mpu6000_rec_buf, sizeof(struct pios_mpu6000_data)) < 0) {
+    uint8_t buf[14] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    uint8_t len     = PIOS_MPU6000_Read(dev->interface_id, dev->slave_num, PIOS_MPU6000_ACCEL_X_OUT_MSB, 14, buf);
+
+    if (len < 14) {
         return false;
     }
-
-    PIOS_MPU6000_Rotate(data, mpu6000_rec_buf);
-#else /* PIOS_MPU6000_I2C_INTERFACE */
-    if (PIOS_MPU6000_ClaimBus() != 0) {
-        return false;
-    }
-
-    static uint8_t mpu6000_send_buf[1 + sizeof(struct pios_mpu6000_data)] = { PIOS_MPU6000_ACCEL_X_OUT_MSB | 0x80, 0, 0, 0, 0, 0, 0, 0, 0 };
-    if (PIOS_SPI_TransferBlock(dev->interface_id, mpu6000_send_buf, mpu6000_rec_buf, sizeof(mpu6000_send_buf), NULL) < 0) {
-        return false;
-    }
-
-    PIOS_MPU6000_ReleaseBus();
-
-    PIOS_MPU6000_Rotate(data, mpu6000_rec_buf + 1);
-#endif /* PIOS_MPU6000_I2C_INTERFACE */
-
+    PIOS_MPU6000_Rotate(data, buf);
     return true;
 }
 
@@ -710,32 +521,17 @@ bool PIOS_MPU6000_ReadFifo(struct pios_mpu6000_data *data)
 
     // Ensure that there's enough data in the FIFO.
     uint16_t depth = PIOS_MPU6000_FifoDepth();
-    if (depth < sizeof(struct pios_mpu6000_data)) {
+    if (depth < 14) {
         return false;
     }
 
     // Read the data out of the FIFO.
-#ifdef PIOS_MPU6000_I2C_INTERFACE
-    if (PIOS_MPU6000_I2CRead(PIOS_MPU6000_FIFO_REG, mpu6000_rec_buf, sizeof(struct pios_mpu6000_data)) < 0) {
+    uint8_t buf[14] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    uint8_t len     = PIOS_MPU6000_Read(dev->interface_id, dev->slave_num, PIOS_MPU6000_FIFO_REG, 14, buf);
+    if (len < 14) {
         return false;
     }
-
-    PIOS_MPU6000_Rotate(data, mpu6000_rec_buf);
-#else /* PIOS_MPU6000_I2C_INTERFACE */
-    if (PIOS_MPU6000_ClaimBus() != 0) {
-        return false;
-    }
-
-    static uint8_t mpu6000_send_buf[1 + sizeof(struct pios_mpu6000_data)] = { PIOS_MPU6000_FIFO_REG | 0x80, 0, 0, 0, 0, 0, 0, 0, 0 };
-    if (PIOS_SPI_TransferBlock(dev->interface_id, mpu6000_send_buf, mpu6000_rec_buf, sizeof(mpu6000_send_buf), NULL) < 0) {
-        return false;
-    }
-
-    PIOS_MPU6000_ReleaseBus();
-
-    PIOS_MPU6000_Rotate(data, mpu6000_rec_buf + 1);
-#endif /* PIOS_MPU6000_I2C_INTERFACE */
-
+    PIOS_MPU6000_Rotate(data, buf);
     return true;
 }
 
